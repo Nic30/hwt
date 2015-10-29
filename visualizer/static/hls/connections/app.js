1807a4045108a0f75adb25aaf5428225888c02ba
Michal Orsák <xorsak@stud.fit.vutbr.cz>
2015-10-29 17:16:19 +0100

    todo update

--- visualizer/static/hls/connections/app.js
+++ visualizer/static/hls/connections/app.js
@@ -1,5 +1,19 @@
 var App = angular.module('App', []);
 /*
+ * https://leanpub.com/D3-Tips-and-Tricks/read
+ * http://www.d3noob.org/2013/03/d3js-force-directed-graph-examples.html
+ * http://blog.pixelingene.com/demos/d3_tree/
+ * http://bl.ocks.org/Neilos/584b9a5d44d5fe00f779
+ * http://www.jointjs.com/tutorial/ports
+ * http://gojs.net/latest/samples/dynamicPorts.html
+ * http://bl.ocks.org/GerHobbelt/3104394
+ * http://bl.ocks.org/mbostock/3681006    --zoom
+ * http://www.codeproject.com/Articles/709340/Implementing-a-Flowchart-with-SVG-and-AngularJS
+ * http://www.coppelia.io/2014/07/an-a-to-z-of-extra-features-for-the-d3-force-layout/
+ * http://bl.ocks.org/explunit/5603250
+ * http://www.ece.northwestern.edu/~haizhou/357/lec6.pdf
+ * http://bl.ocks.org/lgersman/5310854                 -- selection 
+ * 
  * [TODO][Michal] connection to next left boundary of component to allow router make more straight line for long nets
  * [TODO][Zuzana] find way how to allow temporary disable zoom/moving to allow copy of the text 
  * [TODO][Zuzana] javaskriptova funkce ktera snizi jas a kontrast vsech objektu v svg (vse zasedne, a pak dalsi ukol bude pouzit to se zvyraznovanim...)
@@ -12,6 +26,17 @@ var App = angular.module('App', []);
  * [TODO][Marek] add/delete component (prozatim jen predpripraveny json, nebo tak neco bude potreba integrace s filebrowserem)
  * [TODO][Marek] add/delete connection  /construct external port (shortcut), predstava je takova ze kdyz se klikne na sit tak se jakoby nalinkuje na kurzor a kde se klikne tam se pripoji (i vickrat za sebou),
  * 				 zrusi se to pres ESC a pri kliknuti a drzeni napr ctrl se  misto pripojeni vytvori externi port
+ *
+ * [TODO][All] Stranka s demy
+ *
+ *
+ * Prioritne:
+ * 	[Zuzana] filebrowser
+ *	[Marek]  postranni panel ze ktereho se bude dat spustit filebrowser, smazat komponenta, pridat komponenta
+ *	[Michal] otevreni vybrane veci z filebrowser, predpracovat editace
+ *
+ * Proc? 
+ *  Protoze nemuzeme pohodlne prepinat mezi diagramy, a tak to asi tezko dobre otestujeme....
  *
  */
 
