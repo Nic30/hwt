c471ee8e83e11d0b537f58eaa2bfb89d1d648838
zuzanica <zuzanast21@gmail.com>
2015-11-17 22:56:18 +0100

    load folders in filedialog

--- visualizer/static/hls/connections/app.js
+++ visualizer/static/hls/connections/app.js
@@ -94,11 +94,16 @@ App
 							node = node.parent;
 							path = node.data.name + '/' + path;
 						}
-						$scope.selectedFile = path;
-						d3.selectAll("#fileDialog").style({
+						if(node.group){
+							//nacist obsah adresara	
+						}else{
+							$scope.selectedFile = path;
+							d3.selectAll("#fileDialog").style({
 							"display" : "none"
-						});
-						$scope.redraw();
+							});
+							$scope.redraw();	
+						}
+						
 					}
 
 					function sizeCellStyle() {
@@ -161,19 +166,15 @@ App
 						onRowClicked : rowClicked
 					};
 
-					$scope.selectedFile = 'workspace/example1.json';
-					$scope.fileDialog = function() {
-						d3.selectAll("#fileDialog").style({
-							"display" : "block"
-						});
-						$http.get('/hls/connections-data-ls/' + $scope.rootDir)
+					$scope.loadFolderData  = function(path){
+						$http.get('/hls/connections-data-ls/' + path)
 								.then(
 										function(res) {
 											function findDir(path) {
 												return filesRowData;
 											}
 											var files = res.data;
-											var dir = findDir($scope.rootDir);
+											var dir = findDir(path);
 											if (dir.childs === undefined) {
 												files.forEach(function(f) {
 													filesRowData.push(f);
@@ -185,6 +186,15 @@ App
 											$scope.fileGridOptions.api
 													.setRowData(filesRowData);
 										});
+					}
+					
+					$scope.selectedFile = 'workspace/example1.json';
+					$scope.fileDialog = function() {
+						d3.selectAll("#fileDialog").style({
+							"display" : "block"
+						});
+						filesRowData = [];
+						$scope.loadFolderData("");
 					}
 					 $scope.fileDialog()
 					 //$scope.redraw()
