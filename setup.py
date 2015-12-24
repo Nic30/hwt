c0a348221acb2b939ba93d82084a19be99d4594c
Michal Orsák <xorsak@stud.fit.vutbr.cz>
2015-12-24 11:53:32 +0100

    update dependency

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ setup(name='HWToolkit',
       url='https://github.com/Nic30/HWToolkit',
       author='Michal Orsak',
       author_email='michal.o.socials@gmail.com',
+      install_requires=[
+        'slimit', 'sympy',
+        'jinja2', 'ply',
+        'flask'
+      ],
       license='MIT',
-      packages=['hls_toolkit', 'cpp_toolkit', 'python_toolkit', 'tcl_toolkit', 'vivado_toolkit', 'vhdl_toolkit' ],
+      packages=['hls_toolkit', 'cpp_toolkit', 'python_toolkit', 'tcl_toolkit',\
+                'vivado_toolkit', 'vhdl_toolkit' ],
       zip_safe=False)
