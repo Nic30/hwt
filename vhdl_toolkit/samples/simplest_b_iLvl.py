a6be4212eab416e760856e0d4b764ff70290a06a
Michal Orsák <xorsak@stud.fit.vutbr.cz>
2015-12-25 13:57:04 +0100

    unit with Ap_none interfaces

--- vhdl_toolkit/samples/simplest_b_iLvl.py
+++ vhdl_toolkit/samples/simplest_b_iLvl.py
@@ -1,16 +1,12 @@
 from vhdl_toolkit.synthetisator.interfaceLevel.synthetizator import Unit
-from vhdl_toolkit.synthetisator.interfaceLevel.stdInterfaces import Ap_none
 from vhdl_toolkit.formater import formatVhdl
 
 
-
-class SimplestUnit(Unit):
-    a = Ap_none(isExtern=True)
-    b = Ap_none(src=a, isExtern=True)
-
+class SimplestUnit_b(Unit):
+    _origin = 'simplest_b.vhd'
 
 if __name__ == "__main__":
-    u = SimplestUnit()
+    u = SimplestUnit_b()
     print(formatVhdl(
-                     "\n".join([ str(x) for x in u._synthesise("simple")])
+                     "\n".join([ str(x) for x in u._synthesise("SimplestUnit_b")])
                      ))
