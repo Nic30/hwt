60f66cb51ca11b8b9f2551b36936d87c0282ad25
nic30 <nic30@seznam.cz>
2016-02-17 09:21:44 +0100

    find_files- non recursive search

--- python_toolkit/fileHelpers.py
+++ python_toolkit/fileHelpers.py
@@ -2,14 +2,27 @@
 import os, fnmatch
 
 
-def find_files(directory, pattern):
-    for root, dirs, files in os.walk(directory):
-        for basename in files:
+def find_files(directory, pattern, recursive=True):
+    
+    if not os.path.isdir(directory):
+        if os.path.exists(directory):
+            raise IOError(directory + ' is not directory')
+        else:
+            raise IOError(directory + " does not exists")
+    if recursive:
+        for root, _, files in os.walk(directory):
+            for basename in files:
+                if fnmatch.fnmatch(basename, pattern):
+                    filename = os.path.join(root, basename)
+                    yield filename
+    else:
+        root = directory
+        for basename in os.listdir(root):
             if fnmatch.fnmatch(basename, pattern):
                 filename = os.path.join(root, basename)
                 yield filename
-                
-                
+        
+            
 def applyReplacesOnFile(fileName, replaces, write=True):
     with open(fileName) as f:
         content = f.read()
@@ -21,8 +34,3 @@ def applyReplacesOnFile(fileName, replaces, write=True):
             f.write(content)
     else:
         return content
-    
-    
-
-    
-    
