9241406530299a11b133d98138824762a7879060
nic30 <nic30@seznam.cz>
2016-02-17 09:23:36 +0100

    rm unused class

--- vhdl_toolkit/hdlObjects/package.py
+++ vhdl_toolkit/hdlObjects/package.py
@@ -3,16 +3,7 @@ class PackageHeader():
     def __init__(self, name):
         self.name = name
     
-class PackageBody(PackageHeader):
-    pass
-    
-class Package():
-    def __init__(self, header, body):
+class Package(PackageHeader):
+    def __init__(self, name, header):
+        super(Package, self).__init__(name)
         self.header = header
-        self.body = body
-        
-    def getName(self):
-        if self.header:
-            return self.header.name
-        else:
-            return self.body.name
