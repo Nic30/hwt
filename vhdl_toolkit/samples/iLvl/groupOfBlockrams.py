69bd784ebd96723344542bfb6524a8d911019871
nic30 <nic30@seznam.cz>
2016-03-16 00:13:31 +0100

    fix references, width->dtype

--- vhdl_toolkit/samples/iLvl/groupOfBlockrams.py
+++ vhdl_toolkit/samples/iLvl/groupOfBlockrams.py
@@ -1,8 +1,9 @@
 from vhdl_toolkit.synthetisator.interfaceLevel.unit import Unit
 from vhdl_toolkit.formater import formatVhdl
-from vhdl_toolkit.synthetisator.interfaceLevel.interfaces.std import Ap_none
+from vhdl_toolkit.interfaces.std import Ap_none
 from vhdl_toolkit.synthetisator.param import Param, inheritAllParams
 from vhdl_toolkit.samples.iLvl.bram import Bram
+from vhdl_toolkit.hdlObjects.typeShortcuts import vecT
 
 @inheritAllParams
 class GroupOfBlockrams(Unit):
@@ -11,24 +12,24 @@ class GroupOfBlockrams(Unit):
     bramR = Bram()
     bramW = Bram()
     
-    ap_clk = Ap_none(bramR.a.clk, bramR.b.clk,\
-                     bramW.a.clk, bramW.b.clk,\
+    ap_clk = Ap_none(bramR.a.clk, bramR.b.clk, \
+                     bramW.a.clk, bramW.b.clk, \
                      isExtern=True)
-    we = Ap_none(bramR.a.we, bramR.b.we,\
-                 bramW.a.we, bramW.b.we,\
+    we = Ap_none(bramR.a.we, bramR.b.we, \
+                 bramW.a.we, bramW.b.we, \
                  isExtern=True)
-    addr = Ap_none(bramR.a.addr, bramR.b.addr,\
-                   bramW.a.addr, bramW.b.addr,\
-                   width=ADDR_WIDTH, isExtern=True)
-    in_w_a = Ap_none(bramW.a.din, width=DATA_WIDTH, isExtern=True)
-    in_w_b = Ap_none(bramW.b.din, width=DATA_WIDTH, isExtern=True)
-    in_r_a = Ap_none(bramR.a.din, width=DATA_WIDTH, isExtern=True)
-    in_r_b = Ap_none(bramR.b.din, width=DATA_WIDTH, isExtern=True)
+    addr = Ap_none(bramR.a.addr, bramR.b.addr, \
+                   bramW.a.addr, bramW.b.addr, \
+                   dtype=vecT(ADDR_WIDTH), isExtern=True)
+    in_w_a = Ap_none(bramW.a.din, dtype=vecT(DATA_WIDTH), isExtern=True)
+    in_w_b = Ap_none(bramW.b.din, dtype=vecT(DATA_WIDTH), isExtern=True)
+    in_r_a = Ap_none(bramR.a.din, dtype=vecT(DATA_WIDTH), isExtern=True)
+    in_r_b = Ap_none(bramR.b.din, dtype=vecT(DATA_WIDTH), isExtern=True)
     
-    out_w_a = Ap_none(src=bramW.a.dout, width=DATA_WIDTH, isExtern=True)
-    out_w_b = Ap_none(src=bramW.b.dout, width=DATA_WIDTH, isExtern=True)
-    out_r_a = Ap_none(src=bramR.a.dout, width=DATA_WIDTH, isExtern=True)
-    out_r_b = Ap_none(src=bramR.b.dout, width=DATA_WIDTH, isExtern=True)
+    out_w_a = Ap_none(src=bramW.a.dout, dtype=vecT(DATA_WIDTH), isExtern=True)
+    out_w_b = Ap_none(src=bramW.b.dout, dtype=vecT(DATA_WIDTH), isExtern=True)
+    out_r_a = Ap_none(src=bramR.a.dout, dtype=vecT(DATA_WIDTH), isExtern=True)
+    out_r_b = Ap_none(src=bramR.b.dout, dtype=vecT(DATA_WIDTH), isExtern=True)
 
 if __name__ == "__main__":
     u = GroupOfBlockrams()
