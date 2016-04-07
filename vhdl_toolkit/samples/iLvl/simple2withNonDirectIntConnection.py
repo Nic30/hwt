52dc627fa1d5f5387ee2e0bdd41ffcfa585abac8
nic30 <nic30@seznam.cz>
2016-04-07 11:06:59 +0200

    new samle showing acess to inter-sub-unit connections

--- vhdl_toolkit/samples/iLvl/simple2withNonDirectIntConnection.py
+++ vhdl_toolkit/samples/iLvl/simple2withNonDirectIntConnection.py
@@ -5,14 +5,14 @@ from vhdl_toolkit.synthetisator.param import shareAllParams, Param
 
 
 @shareAllParams
-class simple2withNonDirectIntConnection(Unit):
+class Simple2withNonDirectIntConnection(Unit):
     DATA_WIDTH = Param(8)
     a = AxiStream(isExtern=True)
     c = AxiStream(isExtern=True)
     b = AxiStream(c, src=a)
     
 if __name__ == "__main__":
-    u = simple2withNonDirectIntConnection()
+    u = Simple2withNonDirectIntConnection()
     print(formatVhdl(
                      "\n".join([ str(x) for x in u._synthesise()])
                      ))
---
+++ vhdl_toolkit/samples/iLvl/unitToUnitConnection.py
@@ -0,0 +1,24 @@
+from vhdl_toolkit.synthetisator.interfaceLevel.unit import Unit
+from vhdl_toolkit.interfaces.amba import AxiStream
+from vhdl_toolkit.formater import formatVhdl
+from vhdl_toolkit.synthetisator.param import shareAllParams, Param
+from vhdl_toolkit.samples.iLvl.simple2withNonDirectIntConnection import Simple2withNonDirectIntConnection
+from vhdl_toolkit.synthetisator.shortcuts import synthetizeCls
+
+@shareAllParams
+class UnitToUnitConnection(Unit):
+    DATA_WIDTH = Param(8)
+    a = AxiStream(isExtern=True)
+    c = AxiStream(isExtern=True)
+    
+    u0 = Simple2withNonDirectIntConnection()
+    a._endpoints.append(u0.a)
+    #c._setSrc(u0.b)
+    
+    u1 = Simple2withNonDirectIntConnection()
+    b = AxiStream(u1.a, src=u0.b)
+    
+    c._setSrc(u1.b)
+    
+if __name__ == "__main__":
+    print(synthetizeCls(UnitToUnitConnection))
