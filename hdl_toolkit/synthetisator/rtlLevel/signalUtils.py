d9b33a1ca7fcb1defa5cb3148d7674646d85498b
nic30 <nic30@seznam.cz>
2016-04-28 00:34:14 +0200

    automatic bool to bit conversion

--- hdl_toolkit/synthetisator/rtlLevel/signalUtils.py
+++ hdl_toolkit/synthetisator/rtlLevel/signalUtils.py
@@ -1,6 +1,28 @@
 from hdl_toolkit.synthetisator.interfaceLevel.interface import Interface
+from hdl_toolkit.synthetisator.rtlLevel.codeOp import If
+from hdl_toolkit.hdlObjects.typeShortcuts import hBit
+from hdl_toolkit.hdlObjects.typeDefs import BOOL, BIT
+
 def connectSig(a, b):
     if isinstance(a, Interface):
         a = a._sig
     b._src = a
-    return b._sig.assignFrom(a)
+    if isinstance(b, Interface):
+        b = b._sig 
+    
+    if isinstance(b, Interface):
+        bDtype = b._dtype
+    else:
+        bDtype = b.dtype
+        
+    if a.dtype == BOOL and bDtype == BIT:
+        return connectBool2Log(a, b)
+    else:
+        return [b.assignFrom(a)]
+
+def connectBool2Log(a, b):
+    assert(a.dtype == BOOL)
+    return If(a,
+       [b.assignFrom(hBit(1)) ],
+       [b.assignFrom(hBit(0)) ]
+       )
