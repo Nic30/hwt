3309226b779ef6f3863dc495db6214cf7b416811
nic30 <nic30@seznam.cz>
2016-04-27 12:20:51 +0200

    update to new interface _declr, _config etc

--- hls_toolkit/myhdlSynthesiser/__init__.py
+++ hls_toolkit/myhdlSynthesiser/__init__.py
@@ -1,20 +1,25 @@
 
-from myhdl import always_seq,always_comb,  Signal, modbv, ResetSignal, enum
+from myhdl import always_seq, always_comb, Signal, modbv, ResetSignal, enum
 from myhdl.conversion._toVHDL import _ToVHDLConvertor
 import os
 from vhdl_toolkit.hdlObjects.typeDefs import BIT
 
-def toMyHdlInterface(interface):
-    if interface._subInterfaces:
+def toMyHdlInterface(interfaceCls):
+    interface = interfaceCls()
+    interface._loadDeclarations()
+    return _toMyHdlInterface(interface)
+
+def _toMyHdlInterface(interface):
+    if interface._interfaces:
         class MyHdlInterface():
             pass
         
         myhdlIntf = MyHdlInterface()
-        for intfName, intf  in  interface._subInterfaces.items():
-            setattr(myhdlIntf, intfName, toMyHdlInterface(intf))
+        for intf  in  interface._interfaces:
+            setattr(myhdlIntf, intf._name, _toMyHdlInterface(intf))
         return myhdlIntf
     else:
-        #convert type to myhdl signal
+        # convert type to myhdl signal
         t = interface._dtype
         if  t == BIT:
             return Signal(bool(0))
@@ -27,5 +32,5 @@ def convert(u, *params):
     convertor.directory = u.__name__
     os.makedirs(convertor.directory, exist_ok=True)
     convertor(u, *params)
-    with open(os.path.join(convertor.directory, u.__name__+".vhd")) as f :
+    with open(os.path.join(convertor.directory, u.__name__ + ".vhd")) as f :
         print(f.read())
