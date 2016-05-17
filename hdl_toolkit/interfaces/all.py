091445f20eca624112ab0660791b5858e268080d
nic30 <nic30@seznam.cz>
2016-05-17 18:03:03 +0200

    add missing Ap_rst

--- hdl_toolkit/interfaces/all.py
+++ hdl_toolkit/interfaces/all.py
@@ -1,5 +1,6 @@
 from hdl_toolkit.interfaces.std import  BramPort, \
-                 BramPort_withoutClk, Ap_hs, Ap_clk, Ap_rst_n, Ap_none, Ap_vld
+                 BramPort_withoutClk, Ap_hs, Ap_clk, Ap_rst_n, Ap_none, Ap_vld,\
+    Ap_rst
 from hdl_toolkit.interfaces.amba import Axi4, Axi4_xil, \
     AxiLite, AxiLite_xil, AxiStream, AxiStream_withoutSTRB, AxiStream_withUserAndStrb, AxiStream_withUserAndNoStrb 
 
@@ -7,5 +8,5 @@ allInterfaces = [Axi4, Axi4_xil,
    AxiLite, AxiLite_xil,
    BramPort, BramPort_withoutClk,
    AxiStream_withUserAndStrb, AxiStream, AxiStream_withUserAndNoStrb, AxiStream_withoutSTRB,
-   Ap_hs, Ap_clk, Ap_rst_n, Ap_vld, Ap_none
+   Ap_hs, Ap_clk, Ap_rst, Ap_rst_n, Ap_vld, Ap_none
  ]
