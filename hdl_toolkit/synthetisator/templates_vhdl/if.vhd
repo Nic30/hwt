76f636d5af139771fe645d1ce73063579ab7d973
nic30 <nic30@seznam.cz>
2016-05-24 12:24:27 +0200

    elsif syntax fix

--- hdl_toolkit/synthetisator/templates_vhdl/if.vhd
+++ hdl_toolkit/synthetisator/templates_vhdl/if.vhd
@@ -1,6 +1,6 @@
 IF {{ cond }} THEN{% if ifTrue|length >0 %}
     {% for s in ifTrue %}{{s}};
-    {%endfor%}{% endif %}{% if elIfs|length >0 %}{% for c, stms in elIfs %}elsif {{c}}
+    {%endfor%}{% endif %}{% if elIfs|length >0 %}{% for c, stms in elIfs %}ELSIF {{c}} THEN
         {% for s in stms %}{{s}};
     {%endfor%}{%endfor%}{% endif %}{% if ifFalse|length >0 %}ELSE
     {%for s in ifFalse %}{{s}};
