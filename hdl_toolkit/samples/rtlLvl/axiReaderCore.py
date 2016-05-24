da6bca16d6681358a1c1d9804d442c8940977a69
nic30 <nic30@seznam.cz>
2016-05-24 12:12:53 +0200

    new sample

---
+++ hdl_toolkit/samples/rtlLvl/axiReaderCore.py
@@ -0,0 +1,44 @@
+
+from hdl_toolkit.formater import formatVhdl
+from hdl_toolkit.synthetisator.rtlLevel.context import Context
+from hdl_toolkit.synthetisator.rtlLevel.signalUtils import connectSig
+from hdl_toolkit.synthetisator.rtlLevel.codeOp import If
+from hdl_toolkit.hdlObjects.typeDefs import Enum
+
+w = connectSig
+
+
+def axiReaderCore():
+    c = Context("AxiReaderCore")
+    rSt_t = Enum('rSt_t', ['rdIdle', 'rdData'])
+    
+    rSt = c.sig('rSt', rSt_t)
+    arRd = c.sig('arRd')
+    arVld = c.sig('arVld')
+    rVld = c.sig('rVld')
+    rRd = c.sig('rRd')
+
+    # ar fsm next
+    If(arRd,
+       # rdIdle
+        If(arVld,
+           w(rSt_t.rdData, rSt) 
+           ,
+           w(rSt_t.rdIdle, rSt)
+        )
+        ,
+        # rdData
+        If(rRd & rVld,
+           w(rSt_t.rdIdle, rSt)
+           ,
+           w(rSt_t.rdData, rSt) 
+        )
+    )
+    
+    return c, [rSt, arRd, arVld, rVld, rRd]
+    
+if __name__ == "__main__":
+    c, interf = axiReaderCore()
+    
+    for o in c.synthetize(interf):
+            print(formatVhdl(str(o)))
