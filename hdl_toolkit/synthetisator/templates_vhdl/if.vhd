b9144586804bfd1fe92bc2c42d1f00b84fb44297
nic30 <nic30@seznam.cz>
2016-05-24 12:11:50 +0200

    elsifs -> elIfs

--- hdl_toolkit/synthetisator/templates_vhdl/if.vhd
+++ hdl_toolkit/synthetisator/templates_vhdl/if.vhd
@@ -1,6 +1,6 @@
 IF {{ cond }} THEN{% if ifTrue|length >0 %}
     {% for s in ifTrue %}{{s}};
-    {%endfor%}{% endif %}{% if elifs|length >0 %}{% for c, stms in elifs %}elsif {{c}}
+    {%endfor%}{% endif %}{% if elIfs|length >0 %}{% for c, stms in elIfs %}elsif {{c}}
         {% for s in stms %}{{s}};
     {%endfor%}{%endfor%}{% endif %}{% if ifFalse|length >0 %}ELSE
     {%for s in ifFalse %}{{s}};
