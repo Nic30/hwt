ca255106df6523ce2ec5c8813b71e92d11cd279d
nic30 <nic30@seznam.cz>
2016-05-25 23:45:41 +0200

    name actualization

--- hdl_toolkit/intfLvl.py
+++ hdl_toolkit/intfLvl.py
@@ -1,9 +1,8 @@
 from hdl_toolkit.synthetisator.interfaceLevel.interface import Interface
-from hdl_toolkit.synthetisator.interfaceLevel.interface.utils import connect
-
 from hdl_toolkit.synthetisator.interfaceLevel.unit import Unit
 from hdl_toolkit.synthetisator.interfaceLevel.emptyUnit import EmptyUnit
 from hdl_toolkit.synthetisator.interfaceLevel.unitFromHdl import UnitFromHdl
 from hdl_toolkit.synthetisator.interfaceLevel.unitUtils import synthesised
 from hdl_toolkit.synthetisator.param import Param
+from hdl_toolkit.synthetisator.rtlLevel.signal.utils import connect
 
