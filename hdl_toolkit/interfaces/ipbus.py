dcf5b6b22bf302a6c9fd0293eac4ee3f4dffbe3a
nic30 <nic30@seznam.cz>
2016-05-31 20:07:54 +0200

    fix alternativeNames

--- hdl_toolkit/interfaces/ipbus.py
+++ hdl_toolkit/interfaces/ipbus.py
@@ -8,20 +8,20 @@ class IPBus(Interface):
     def _config(self):
         self.REG_COUNT = Param(1)
     def _declr(self):
-        self.bus2ip_addr = s(dtype=vecT(32)) 
-        self.bus2ip_data = s(dtype=vecT(32))
-        self.bus2ip_be = s(dtype=vecT(4))
-        self.bus2ip_rnw = s()
-        self.bus2ip_cs = s()
+        self.bus2ip_addr = s(dtype=vecT(32), alternativeNames=["b2i_addr"]) 
+        self.bus2ip_data = s(dtype=vecT(32), alternativeNames=["b2i_data"])
+        self.bus2ip_be = s(dtype=vecT(4), alternativeNames=["b2i_be"])
+        self.bus2ip_rnw = s(alternativeNames=["b2i_rnw"])
+        self.bus2ip_cs = s(alternativeNames=["b2i_cs"])
 
-        self.ip2bus_data = s(dtype=vecT(32), masterDir=DIRECTION.IN) 
-        self.ip2bus_wrack = s(masterDir=DIRECTION.IN)
-        self.ip2bus_rdack = s(masterDir=DIRECTION.IN)
-        self.ip2bus_error = s(masterDir=DIRECTION.IN)
+        self.ip2bus_data = s(dtype=vecT(32), masterDir=DIRECTION.IN, alternativeNames=["i2b_data"]) 
+        self.ip2bus_wrack = s(masterDir=DIRECTION.IN, alternativeNames=["i2b_wrack"])
+        self.ip2bus_rdack = s(masterDir=DIRECTION.IN, alternativeNames=["i2b_rdack"])
+        self.ip2bus_error = s(masterDir=DIRECTION.IN, alternativeNames=["i2b_error"])
         
         
 class IPbusWithCE(IPBus):
     def _declr(self):
         super()._declr()
-        self.bus2ip_rdce = s(dtype=vecT(self.REG_COUNT))
-        self.bus2ip_wrce = s(dtype=vecT(self.REG_COUNT))
+        self.bus2ip_rdce = s(dtype=vecT(self.REG_COUNT), alternativeNames=["b2i_rdce"])
+        self.bus2ip_wrce = s(dtype=vecT(self.REG_COUNT), alternativeNames=["b2i_wrce"])
