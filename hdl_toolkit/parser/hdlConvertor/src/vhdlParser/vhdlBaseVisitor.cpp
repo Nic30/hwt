549026e67c0f9a261184aecfc513f61c26c0a958
nic30 <nic30@seznam.cz>
2016-06-20 20:04:21 +0200

    parser modules restructuralization

--- hdl_toolkit/parser/hdlConvertor/src/vhdlParser/vhdlBaseVisitor.cpp
+++
@@ -1,9 +0,0 @@
-
-// Generated from vhdl.g4 by ANTLR 4.5.3
-
-
-#include "vhdlBaseVisitor.h"
-
-
-using namespace vhdlParser;
-
--- hdl_toolkit/parser/hdlConvertor/src/vhdlParser/vhdlVisitor.cpp
+++
@@ -1,9 +0,0 @@
-
-// Generated from vhdl.g4 by ANTLR 4.5.3
-
-
-#include "vhdlVisitor.h"
-
-
-using namespace vhdlParser;
-
