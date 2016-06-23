e747fc42645853f1c9ebc5e58aa1538df34f915f
nic30 <nic30@seznam.cz>
2016-06-23 19:24:18 +0200

    parallel build

--- hdl_toolkit/parser/hdlConvertor/setup.py
+++ hdl_toolkit/parser/hdlConvertor/setup.py
@@ -2,6 +2,7 @@ import os
 from distutils.core import setup, Extension
 from distutils.command.build_ext import build_ext
 from distutils.sysconfig import customize_compiler
+import distutils.ccompiler
 
 class buildWithoutStrictPrototypes(build_ext):
     def build_extensions(self):
@@ -11,6 +12,25 @@ class buildWithoutStrictPrototypes(build_ext):
         except (AttributeError, ValueError):
             pass
         build_ext.build_extensions(self)
+
+# monkey-patch for parallel compilation
+def parallelCCompile(self, sources, output_dir=None, macros=None, include_dirs=None,
+                      debug=0, extra_preargs=None, extra_postargs=None, depends=None):
+    # those lines are copied from distutils.ccompiler.CCompiler directly
+    macros, objects, extra_postargs, pp_opts, build = self._setup_compile(
+            output_dir, macros, include_dirs, sources, depends, extra_postargs)
+    cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
+    # parallel code
+    import multiprocessing.pool
+    N= multiprocessing.cpu_count() # number of parallel compilations
+    def _single_compile(obj):
+        try: src, ext = build[obj]
+        except KeyError: return
+        self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
+    # convert to list, imap is evaluated on-demand
+    list(multiprocessing.pool.ThreadPool(N).imap(_single_compile,objects))
+    return objects
+distutils.ccompiler.CCompiler.compile=parallelCCompile
 
 
 BASE = "src/"
