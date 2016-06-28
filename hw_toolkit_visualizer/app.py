0ff81364608f391d252b4c2da4222f6e1de52702
nic30 <nic30@seznam.cz>
2016-06-28 15:35:07 +0200

    refactoring of dependencyView

--- hw_toolkit_visualizer/app.py
+++ hw_toolkit_visualizer/app.py
@@ -1,10 +1,9 @@
-import os, sys
+import sys
 from flask import Flask, render_template
 from flask.helpers import send_from_directory
-
-from hdl_toolkit.hierarchyExtractor import DesignFile
 from hls_connections_views import connectionsBp
-from python_toolkit.fileHelpers import find_files
+from dependencyViews import dependenciesBp
+
 
 sys.path.append("..")  # [hotfix] to make visualizer run after downloading from git
 
@@ -28,47 +27,8 @@ def index():
 def gantt():
     return render_template('hls/gantt_chart.html', ganttTasks=[], ganttTaskNames=[])
 
-
-# http://www.coppelia.io/2014/07/an-a-to-z-of-extra-features-for-the-d3-force-layout/
-@app.route('/dependency')
-def dependencyGraph():
-    def convertToRelativePaths(root, depDict):
-        outDict = {}
-        for f, dep in depDict.items():
-            f = os.path.relpath(f, root)
-            depSet = set()
-            outDict[f] = depSet
-            for d in dep:
-                depSet.add(os.path.relpath(d, root))
-        return outDict
-    #unisimFiles = ['/opt/Xilinx/Vivado/2015.2/data/vhdl/src/unisims/retarget_VCOMP.vhd']
-    # workspace = "/home/nic30/Downloads/fpgalibs/src/hfer/"
-    workspace = "/home/nic30/Downloads/fpgalibs/src/"
-    files = []
-    # files.extend(find_files(workspace, '*.vhd'))
-    files.extend(list(find_files(workspace + 'util/', '*.vhd')) 
-               + list(find_files(workspace + 'axi/', '*.vhd')))
-    #unisimDesFiles = DesignFile.loadFiles(unisimFiles, libName='unisim')
-    workDesFiles = DesignFile.loadFiles(files)
-    depDict = DesignFile.fileDependencyDict(workDesFiles) # + unisimDesFiles)
-    depDict = convertToRelativePaths(workspace, depDict)
-    nodes = []
-    links = []
-    indexes = {}
-    indx = 0
-    for file in depDict:
-        indexes[file] = indx
-        indx += 1
-        nodes.append({"name": file, 'group': 1})
-
-    for file, connections in depDict.items():
-        for c in connections:
-            links.append({"source": indexes[file],
-                          'target': indexes[c],
-                          "value": 1})
-
-    return render_template('hls/dependency.html', nodes=nodes, links=links)
 app.register_blueprint(connectionsBp)
+app.register_blueprint(dependenciesBp)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ hw_toolkit_visualizer/dependencyViews.py
@@ -0,0 +1,53 @@
+import os
+from flask.blueprints import Blueprint
+from hdl_toolkit.hierarchyExtractor import DesignFile
+from python_toolkit.fileHelpers import find_files
+from flask import render_template
+from hdl_toolkit.parser.loader import ParserFileInfo
+
+
+dependenciesBp = Blueprint('dependencies', __name__, template_folder='templates/dependencies/')
+
+
+
+# http://www.coppelia.io/2014/07/an-a-to-z-of-extra-features-for-the-d3-force-layout/
+@dependenciesBp.route('/dependencies/')
+def dependencyGraph():
+    def convertToRelativePaths(root, depDict):
+        outDict = {}
+        for f, dep in depDict.items():
+            f = os.path.relpath(f, root)
+            depSet = set()
+            outDict[f] = depSet
+            for d in dep:
+                depSet.add(os.path.relpath(d, root))
+        return outDict
+    #unisimFiles = ['/opt/Xilinx/Vivado/2015.2/data/vhdl/src/unisims/retarget_VCOMP.vhd']
+    # workspace = "/home/nic30/Downloads/fpgalibs/src/hfer/"
+    workspace = "/home/nic30/Documents/workspace/sprobe10/fpgalibs/src/"
+    files = []
+    # files.extend(find_files(workspace, '*.vhd'))
+    files.extend(list(find_files(workspace + 'util/', '*.vhd')) 
+               + list(find_files(workspace + 'axi/', '*.vhd')))
+    
+    files = list(map(lambda f : ParserFileInfo(f, "work"), files ))
+    #unisimDesFiles = DesignFile.loadFiles(unisimFiles, libName='unisim')
+    workDesFiles = DesignFile.loadFiles(files)
+    depDict = DesignFile.fileDependencyDict(workDesFiles) # + unisimDesFiles)
+    depDict = convertToRelativePaths(workspace, depDict)
+    nodes = []
+    links = []
+    indexes = {}
+    indx = 0
+    for file in depDict:
+        indexes[file] = indx
+        indx += 1
+        nodes.append({"name": file, 'group': 1})
+
+    for file, connections in depDict.items():
+        for c in connections:
+            links.append({"source": indexes[file],
+                          'target': indexes[c],
+                          "value": 1})
+
+    return render_template('hls/dependency.html', nodes=nodes, links=links)
