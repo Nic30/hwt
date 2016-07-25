ce64c16f99d256bfa7d4314abbc97de2fcfda109
nic30 <nic30@seznam.cz>
2016-07-25 19:21:41 +0200

    fix non determinism in applyValues triggering

--- hdl_toolkit/simulator/hdlSimulator.py
+++ hdl_toolkit/simulator/hdlSimulator.py
@@ -1,19 +1,26 @@
+import math
 import simpy
+
 from hdl_toolkit.hdlObjects.value import Value
 from hdl_toolkit.simulator.hdlSimConfig import HdlSimConfig
 from hdl_toolkit.synthetisator.interfaceLevel.mainBases import InterfaceBase
-from hdl_toolkit.hdlObjects.types.typeCast import toHVal
-from math import inf
-import math
+
+
 
 class HdlSimulator(object):
+    """
+    while True:
+        apply values on signals
+        run all processes which were tiggered
+    """
+    
     # http://heather.cs.ucdavis.edu/~matloff/156/PLN/DESimIntro.pdf
     ps = 1
     ns = 1000
     us = ns * 1000
     ms = us * 1000
     s = ms * 1000
-    
+
     def __init__(self, config=None):
         if config is None:
             config = HdlSimConfig() 
@@ -21,51 +28,55 @@ class HdlSimulator(object):
         self.env = simpy.Environment()
         self.updateComplete = self.env.event()
         self.lastUpdateComplete = -1
+        self.applyValuesPlaned = False
         
         # (signal, value) tupes which should be applied before new round of processes
         #  will be executed
         self.valuesToApply = []
     
     def addHwProcToRun(self, proc):
-        if not self.valuesToApply:
+        # first process in time has to plan executing of apply values on the end of this time
+        if not self.applyValuesPlaned:
             # (apply in future)
             self.env.process(self.applyValues())
-            
+            self.applyValuesPlaned = True
+
         for v in proc.simEval(self):
             self.valuesToApply.append(v)
     
     def applyValues(self):
         # [TODO] not ideal, processes should be evaluated before runing apply values
         # this should be done by priority, not by timeout
+        # (currently cant gen scipy working with priorities)
         yield self.wait(0)
-        
-        updatedSigs = {}
         va = self.valuesToApply
+        
+        # log if there are items to log
         if va and self.config.logApplyingValues:
             self.config.logApplyingValues(self, va)
             
         self.valuesToApply = []
 
+        # apply values to signals, values can overwrite each other
+        # but each signal should be driven by only one process and
+        # it should resolve value collision
         for s, v in va:
             v.updateTime = self.env.now
-            try:
-                lastV = updatedSigs[s]
-            except KeyError:
-                lastV = None
+            s.simUpdateVal(self, v)
                 
-            if lastV is not None:
-                # short circuit if lastV is different
-                raise NotImplementedError()
-            else:
-                s.simUpdateVal(self, v)
-                
-                
-        now = self.env.now
+        # processes triggered from simUpdateVal can add nev values
+        if self.valuesToApply:
+            yield from self.applyValues()
+        
+        # activate updateComplete if this was last applyValues() in this time        
         nextEventT = self.env.peek()
+        now = self.env.now
         # is last event or is last in this time
-        if math.isinf(nextEventT) or (nextEventT > now and not self.valuesToApply) and self.lastUpdateComplete < now:
-            self.updateComplete.succeed() # trigger
-            self.updateComplete = self.env.event() # regenerate event
+        if (math.isinf(nextEventT) or nextEventT > now\
+            and self.lastUpdateComplete < now:
+            
+            self.updateComplete.succeed()  # trigger
+            self.updateComplete = self.env.event()  # regenerate event
             self.lastUpdateComplete = now
             
     def _initSignals(self, signals):
@@ -109,7 +120,7 @@ class HdlSimulator(object):
         v.updateTime = self.env.now
         if isinstance(sig, InterfaceBase):
             sig = sig._sigInside
-        assert isinstance(v, Value)
+
         v = v._convert(sig._dtype)
         
         sig.simUpdateVal(self, v)
