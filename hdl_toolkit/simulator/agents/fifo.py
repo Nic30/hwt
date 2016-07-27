84aeb4a00d83d9cc5c0da198de06c7bdf9b5a500
nic30 <nic30@seznam.cz>
2016-07-28 00:46:24 +0200

    partial fix: monitor - first is resolved as non valid, but in sim it looks ok.

--- hdl_toolkit/simulator/agents/fifo.py
+++ hdl_toolkit/simulator/agents/fifo.py
@@ -1,61 +1,24 @@
-from hdl_toolkit.simulator.agents.agentBase import AgentBase
-from hdl_toolkit.simulator.shortcuts import afterRisingEdge
+from hdl_toolkit.simulator.agents.agentBase import SyncAgentBase
 
-class FifoReaderAgent(AgentBase):
-    def __init__(self, intf, clk=None, rstn=None):
-        self.intf = intf
-        
-        p = intf._parent
-        if clk is None:
-            self.clk = p.clk
-        else:
-            self.clk = clk
-        if rstn is None:
-            self.rst_n = p.rst_n 
-        else:
-            self.rst_n = rstn
-
-        self.enable = True
-        self.data = []
-        
-        self.monitor = afterRisingEdge(self.clk, self.monitor)
-        self.driver = afterRisingEdge(self.clk, self.driver)
-        
+class FifoReaderAgent(SyncAgentBase):
+       
     def monitor(self, s):
         intf = self.intf
         
         if s.r(self.rst_n).val and self.enable:
             rd = not s.r(intf.wait).val
+            s.w(rd, intf.en)
             if rd:
+                yield s.wait(0) # let rest of the system act
                 d = s.read(intf.data)
                 self.data.append(d)
-            s.w(rd, intf.en)
         else:
             s.w(0, intf.en)
             
     def driver(self, s):
         raise NotImplementedError()
 
-class FifoWriterAgent(AgentBase):
-    def __init__(self, intf, clk=None, rstn=None):
-        self.intf = intf
-        p = intf._parent
-        
-        if clk is None:
-            self.clk = p.clk
-        else:
-            self.clk = clk
-            
-        if rstn is None:
-            self.rst_n = p.rst_n 
-        else:
-            self.rst_n = rstn
-
-        self.enable = True
-        self.data = []
-        
-        self.monitor = afterRisingEdge(self.clk, self.monitor)
-        self.driver = afterRisingEdge(self.clk, self.driver)
+class FifoWriterAgent(SyncAgentBase):
         
     def monitor(self, s):
         raise NotImplementedError()
