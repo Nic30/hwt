721717ae796cfbeec2788515f57ddf3a27106135
nic30 <nic30@seznam.cz>
2016-08-06 13:15:49 +0200

    extract get rst, clk to functions

--- hdl_toolkit/simulator/agents/agentBase.py
+++ hdl_toolkit/simulator/agents/agentBase.py
@@ -17,9 +17,33 @@ class AgentBase():
     
     def monitor(self, s):
         raise NotImplementedError()
-    
+
+   
 class SyncAgentBase(AgentBase):
-    def __init__(self, intf, clk=None, rstn=None):
+    def getRst_n(self, unit, allowNoReset):
+        try:
+            return unit.rst_n 
+        except AttributeError:
+            pass
+        
+        try:
+            return ~unit.rst 
+        except AttributeError:
+            pass
+        
+        if allowNoReset:
+            return None
+        else:
+            raise Exception("Can not find reset on unit %s" % (repr(unit)))
+        
+    
+    def notReset(self, s):
+        if self.rst_n is None:
+            return True
+        else:
+            s.r(self.rst_n).val
+        
+    def __init__(self, intf, clk=None, rstn=None, allowNoReset=False):
         super().__init__(intf)
         
         # resolve clk and rstn
@@ -28,8 +52,9 @@ class SyncAgentBase(AgentBase):
             self.clk = p.clk
         else:
             self.clk = clk
+            
         if rstn is None:
-            self.rst_n = p.rst_n 
+            self.rst_n = self.getRst_n(p, allowNoReset=allowNoReset)
         else:
             self.rst_n = rstn
 
