4ae399509f085cc852ded971ffc1daf9c7c71f80
nic30 <nic30@seznam.cz>
2016-08-08 16:31:26 +0200

    fix: dataType for modelParametes

--- cli_toolkit/ip_packager/port.py
+++ cli_toolkit/ip_packager/port.py
@@ -7,6 +7,7 @@ from cli_toolkit.ip_packager.helpers import appendSpiElem, \
          findS, mkSpiElm, ns
 from cli_toolkit.ip_packager.exprSerializer import VivadoTclExpressionSerializer
 from hdl_toolkit.hdlObjects.types.bits import Bits
+from hdl_toolkit.serializer.vhdlSerializer import VhdlSerializer
 
 class WireTypeDef():
     _requiredVal = ["typeName"]
@@ -55,14 +56,17 @@ class Port():
         t = port.type
         dt = p._dtype
         
-        t.typeName = dt.name.upper()
+        
+        t.typeName = VhdlSerializer.HdlType(dt)
+        try:
+            t.typeName = t.typeName[:t.typeName.index('(')]
+        except ValueError:
+            pass
+        
         if dt == BIT:
             port.vector = False
         elif isinstance(dt, Bits):
-            c = dt.constrain.origin
-            assert(isinstance(c, Operator) and c.operator == AllOps.DOWNTO)
-            
-            port.vector = (c.ops[0], c.ops[1])
+            port.vector = dt.constrain.staticEval().val
         t.viewNameRefs = ["xilinx_vhdlsynthesis", "xilinx_vhdlbehavioralsimulation"]
         return port
     
