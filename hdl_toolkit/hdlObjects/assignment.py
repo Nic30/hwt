499e6c7774089cbb479321da0dc4d7b99c29e722
nic30 <nic30@seznam.cz>
2016-08-27 19:06:05 +0200

    new: instance id for assingment for better sorting in hdl

--- hdl_toolkit/hdlObjects/assignment.py
+++ hdl_toolkit/hdlObjects/assignment.py
@@ -29,13 +29,27 @@ class Assignment():
     @ivar indexes: description of index selector on dst (list of Index/Slice objects)
                     (f.e. [[0], [1]] means  dst[0][1]  )
     
+    @cvar __instCntr: counter used for generating instance ids
+    @ivar _instId: internaly used only for intuitive sorting of statements
     """
+    __instCntr = 0
+    
     def __init__(self, src, dst, indexes=None):
         self.src = src
         self.dst = dst
         self.isEventDependent = False
         self.indexes = indexes
         self.cond = set()
+        self._instId = Assignment._nextInstId()
+    
+    @classmethod
+    def _nextInstId(cls):
+        """
+        Get next instance id
+        """
+        i = cls.__instCntr
+        cls.__instCntr +=1
+        return i
         
     def seqEval(self):
         self.dst._val = self.src.staticEval() 
