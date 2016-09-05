30381f6b26b7bc28b7af6ae9a87dab8485f0a394
nic30 <nic30@seznam.cz>
2016-09-05 17:13:02 +0200

    rm axi interfaces because they are in separate library

--- hdl_toolkit/interfaces/all.py
+++ hdl_toolkit/interfaces/all.py
@@ -1,12 +1,12 @@
 from hdl_toolkit.interfaces.std import  BramPort, \
                  BramPort_withoutClk, Handshaked, Clk, Rst_n, Signal, Rst, RdSynced, VldSynced
-from hdl_toolkit.interfaces.amba import Axi4, Axi4_xil, \
-    AxiLite, AxiLite_xil, AxiStream, AxiStream_withoutSTRB, AxiStream_withUserAndStrb, \
-    AxiStream_withUserAndNoStrb 
+#from hwtLib.interfaces.amba import Axi4, Axi4_xil, \
+#    AxiLite, AxiLite_xil, AxiStream, AxiStream_withoutSTRB, AxiStream_withUserAndStrb, \
+#    AxiStream_withUserAndNoStrb 
 
-allInterfaces = [Axi4, Axi4_xil,
-   AxiLite, AxiLite_xil,
+allInterfaces = [#Axi4, Axi4_xil,
+   #AxiLite, AxiLite_xil,
    BramPort, BramPort_withoutClk,
-   AxiStream_withUserAndStrb, AxiStream, AxiStream_withUserAndNoStrb, AxiStream_withoutSTRB,
+   #AxiStream_withUserAndStrb, AxiStream, AxiStream_withUserAndNoStrb, AxiStream_withoutSTRB,
    Handshaked,  Clk, Rst, Rst_n, VldSynced, RdSynced, Signal
  ]
