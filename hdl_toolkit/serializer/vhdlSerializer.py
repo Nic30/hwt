fcfe5e69503d3f1eb451cbc4c9ce0f8526d80eb0
nic30 <nic30@seznam.cz>
2016-09-11 23:16:49 +0200

    fix slice vs sliceval mismatch

--- hdl_toolkit/serializer/vhdlSerializer.py
+++ hdl_toolkit/serializer/vhdlSerializer.py
@@ -28,6 +28,7 @@ from hdl_toolkit.serializer.utils import maxStmId
 from hdl_toolkit.hdlObjects.types.boolean import Boolean
 from hdl_toolkit.hdlObjects.types.integer import Integer
 from hdl_toolkit.hdlObjects.types.string import String
+from hdl_toolkit.hdlObjects.types.slice import Slice
 
 
 VHLD_KEYWORDS = [
@@ -575,7 +576,10 @@ class VhdlSerializer():
         @param dst: is signal connected with value 
         @param val: value object, can be instance of Signal or Value    """
         t = val._dtype
-        if isinstance(t, Array):
+        
+        if isinstance(t, Slice):
+            return cls.Slice_valAsVhdl(t, val)
+        elif isinstance(t, Array):
             return cls.Array_valAsVhdl(t, val)
         elif isinstance(t, Bits):
             return cls.Bits_valAsVhdl(t, val)
@@ -585,15 +589,13 @@ class VhdlSerializer():
             return cls.Enum_valAsVhdl(t, val)
         elif isinstance(t, Integer):
             return cls.Integer_valAsVhdl(t, val)
-        elif isinstance(t, SliceVal):
-            return cls.Slice_valAsVhdl(t, val)
         elif isinstance(t, String):
             return cls.String_valAsVhdl(t, val)
         elif isinstance(val, RtlSignalBase):
             return cls.SignalItem(val)
         else:
             raise Exception("value2vhdlformat can not resolve value serialization for %s" % (repr(val))) 
-        
+            
     @classmethod
     def BitToBool(cls, cast):
         v = 0 if cast.sig.negated else 1
