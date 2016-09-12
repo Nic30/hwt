4f69b8d7f81b3715bb37a80a1a995092a63ff1e7
nic30 <nic30@seznam.cz>
2016-09-12 22:53:40 +0200

    new: SimBitsT, fix sensitivity() fn

--- hdl_toolkit/simulator/simModel.py
+++ hdl_toolkit/simulator/simModel.py
@@ -1,11 +1,50 @@
+from hdl_toolkit.hdlObjects.types.bits import Bits
+from hdl_toolkit.bitmask import Bitmask
+
+__simBitsTCache = {}
+def simBitsT(width, signed):
+    """
+    Construct SimBitsT with cache
+    """
+    k = (width, signed)
+    try:
+        return __simBitsTCache[k] 
+    except KeyError:
+        t = SimBitsT(width, signed)
+        __simBitsTCache[k] = t
+        return t
+    
+
+class SimBitsT(Bits):
+    """
+    Simplified Bits type for simulation purposes
+    """
+    def __init__(self, widthConstr, signed):
+        self.constrain = widthConstr
+        self.signed = signed
+        self._allMask = Bitmask.mask(self.bit_length())
+    
+    def __eq__(self, other):
+        return isinstance(other, Bits) and other.bit_length() == self.bit_length()\
+            and self.signed == other.signed
+    
+    def __hash__(self):
+        return hash((self.constrain, self.signed))
+    
+    def all_mask(self):
+        return self._allMask
+    
+    def bit_length(self):
+        return self.constrain
 
 
 def sensitivity(proc, *sensitiveTo):
     """
     register sensitivity for process
-    and bound process int
     """
-    proc.sensitivityList = sensitiveTo 
+    for s in sensitiveTo:
+        s.simSensitiveProcesses.add(proc)
+         
 
 class SimModel(object):
     pass
