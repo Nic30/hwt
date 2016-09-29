64b518b6d1f1f55b3f18bbd64204f994acdfaeda
nic30 <nic30@seznam.cz>
2016-09-29 13:53:25 +0200

    replace switch with if-tree

--- hdl_toolkit/serializer/templates_simModel/switch.py
+++
@@ -1,8 +0,0 @@
-{% for c in cases %}{{indent}}{% if c[0] is not none %}_cond, _condVldTmp = simEvalCond(sim, {{ switchOn }}._eq({{c[0]}}))
-{{indent}}_condVld = _condVld and _condVldTmp
-{{indent}}if _cond or not _condVld:{% for stm in c[1] %}
-{{stm}}{% endfor %}
-{{indent}}    raise StopIteration()
-{% else %}{% for stm in c[1] %}
-{{stm}}{% endfor %}
-{%endif%}{% endfor %}
