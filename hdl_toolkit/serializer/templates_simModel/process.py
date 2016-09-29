d3c733f59e8fcc5848769cbeaab831d8c38e5130
nic30 <nic30@seznam.cz>
2016-09-29 13:53:40 +0200

    update of templat

--- hdl_toolkit/serializer/templates_simModel/process.py
+++ hdl_toolkit/serializer/templates_simModel/process.py
@@ -1,4 +1,4 @@
     # sensitivity: {{sensitivityList|join(", ")}}
     def {{name}}(self, sim):
-        _condVld = True{% for stmLine in stmLines %}
+        cVld_1 = True{% for stmLine in stmLines %}
 {{ stmLine }}{% endfor %}
