a7225bcb6845965ed2438457b09d9e1949914420
nic30 <nic30@seznam.cz>
2016-12-18 18:53:52 +0100

    rm deprecated from HWProcess

--- hdl_toolkit/hdlObjects/process.py
+++ hdl_toolkit/hdlObjects/process.py
@@ -5,17 +5,8 @@ class HWProcess():
     """
     def __init__(self, name):
         self.name = name
-        self.interfaces = {}
         self.statements = []
         self.sensitivityList = set()
-
-    def simEval(self, simulator):
-        """
-        Called by simulator when signal has changed value and this process
-        should be recounted
-        """
-        for s in self.statements:
-            yield from s.simEval(simulator)
 
     def __repr__(self):
         from hdl_toolkit.serializer.vhdl.serializer import VhdlSerializer
