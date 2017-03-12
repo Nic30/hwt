e9b4d77ee7630de5a391a41bdbd04028d14ed1b7
nic30 <nic30@seznam.cz>
2017-03-12 12:52:25 +0100

    codestyle: rm whitespaces

--- hwt/serializer/ip_packager/interfaces/intfConfig.py
+++ hwt/serializer/ip_packager/interfaces/intfConfig.py
@@ -7,29 +7,32 @@ from hwt.pyUtils.arrayQuery import single
 
 DEFAULT_CLOCK = 100000000
 
+
 class Type():
     __slots__ = ['name', 'version', 'vendor', 'library']
-     
+
     @classmethod 
     def fromElem(cls, elm):
         self = cls()
         for s in ['name', 'version', 'vendor', 'library']:
             setattr(self, s, elm.attrib[spi_ns_prefix + s])
         return self
-    
+
     def asElem(self, elmName):
         e = mkSpiElm(elmName)
         for s in ['name', 'version', 'vendor', 'library']:
             e.attrib[spi_ns_prefix + s] = getattr(self, s)
-        return e    
-         
+        return e
+
+
 class IntfConfig(Type):
     def __init__(self):
         self.parameters = []
         self.map = {}
+
     def findPort(self, logName):
         logName = logName.lower()
-        p = single(self.port, lambda x : x.logName.lower() == logName)
+        p = single(self.port, lambda x: x.logName.lower() == logName)
         return p
 
     def addSimpleParam(self, thisIntf, name, value):
@@ -40,15 +43,15 @@ class IntfConfig(Type):
         p.value.text = value
         self.parameters.append(p)
         return p
-    
+
     def addWidthParam(self, thisIntf, name, value):
         def createTmpVar(suggestedName, dtype):
             raise NotImplementedError("Value of generic %s can not be converted do ipcore format (%s)", name, repr(value))
-       
+
         p = self.addSimpleParam(thisIntf, "ADDR_WIDTH",
-                            VivadoTclExpressionSerializer.asHdl(value.staticEval(), createTmpVar))
+                                VivadoTclExpressionSerializer.asHdl(value.staticEval(), createTmpVar))
         if isinstance(value, RtlSignalBase):
-            p.value.resolve = "user" 
-        
+            p.value.resolve = "user"
+
     def postProcess(self, component, entity, allInterfaces, thisIf):
-        pass 
+        pass
