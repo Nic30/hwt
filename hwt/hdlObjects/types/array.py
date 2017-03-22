deca730381faeae9ce385433c6953aaf76cbdf33
nic30 <nic30@seznam.cz>
2017-03-22 11:37:31 +0100

    codestyle: pep8

--- hwt/hdlObjects/types/array.py
+++ hwt/hdlObjects/types/array.py
@@ -1,4 +1,5 @@
-from hwt.hdlObjects.types.hdlType import HdlType 
+from hwt.hdlObjects.types.hdlType import HdlType
+
 
 class Array(HdlType):
     """
@@ -9,15 +10,15 @@ class Array(HdlType):
         super(Array, self).__init__()
         self.elmType = elmType
         self.size = size
-    
+
     def __hash__(self):
         return hash((self.elmType, self.size))
-    
+
     @classmethod
     def getValueCls(cls):
         try:
             return cls._valCls
         except AttributeError:
-            from hwt.hdlObjects.types.arrayVal import ArrayVal 
+            from hwt.hdlObjects.types.arrayVal import ArrayVal
             cls._valCls = ArrayVal
             return cls._valCls
--- hwt/hdlObjects/types/sliceVal.py
+++ hwt/hdlObjects/types/sliceVal.py
@@ -1,6 +1,7 @@
-from hwt.hdlObjects.types.arrayVal import ArrayVal  
+from hwt.hdlObjects.types.arrayVal import ArrayVal
 from hwt.hdlObjects.types.defs import SLICE
 from hwt.hdlObjects.value import Value
+
 
 class SliceVal(ArrayVal):
 
@@ -9,11 +10,10 @@ class SliceVal(ArrayVal):
         _1 = self.val[1].staticEval()
         updateTime = max(_0.updateTime, _1.updateTime)
         return SliceVal([_0, _1], SLICE, None, updateTime)
-    
+
     def _isFullVld(self):
         return self.val[0]._isFullVld() and self.val[1]._isFullVld()
 
     def _size(self):
         assert isinstance(self, Value)
-        
         return self.val[0].val - self.val[1].val +1
