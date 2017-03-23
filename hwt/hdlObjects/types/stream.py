3de5bbd44a27bda727c3879e93f434c4bdc88811
nic30 <nic30@seznam.cz>
2017-03-23 18:12:44 +0100

    new: hld type HStream

---
+++ hwt/hdlObjects/types/stream.py
@@ -0,0 +1,18 @@
+from hwt.hdlObjects.types.hdlType import HdlType
+
+
+class HStream(HdlType):
+    """
+    Stream data type
+    stream is like array with unlimited size
+    """
+    def __init__(self, elmType):
+        """
+        @param elmType: type of item used for alignment purposes
+        @attention: width of itemType and width of interface over which
+                    is this stream send are two different things
+        """
+        self.elmType = elmType
+
+    def __hash__(self):
+        return hash(self.elmType)
