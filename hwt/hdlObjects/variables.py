4baa91eea68c1fff6c04822cc23a42d8f05722c2
nic30 <nic30@seznam.cz>
2017-04-19 15:45:38 +0200

    codestyle

--- hwt/hdlObjects/variables.py
+++ hwt/hdlObjects/variables.py
@@ -7,7 +7,7 @@ class SignalItem(object):
         """
         :param name: name for better orientation in netlists (used only in serialization)
         :param dtype: data type of this signal
-        :param defaultVal: value for initialization 
+        :param defaultVal: value for initialization
         :param virtualOnly: flag indicates that this assignments is only virtual and should not be added into
             netlist, because it is only for internal notation
         """
@@ -15,22 +15,19 @@ class SignalItem(object):
         self._dtype = dtype
         self.virtualOnly = virtualOnly
         if defaultVal is None:
-            defaultVal = dtype.fromPy(None) 
+            defaultVal = dtype.fromPy(None)
         self.defaultVal = defaultVal
         self._setDefValue()
-        
+
     def _setDefValue(self):
         v = self.defaultVal
         if isinstance(v, RtlSignalBase):
             v = v.staticEval()
-            
+
         self._val = v.clone()
         self._oldVal = self._val.clone()
         self._oldVal.vldMask = 0
-    
+
     def __repr__(self):
         from hwt.serializer.vhdl.serializer import VhdlSerializer, onlyPrintDefaultValues
-        return VhdlSerializer.SignalItem(self, onlyPrintDefaultValues)        
-    
-        
-        
+        return VhdlSerializer.SignalItem(self, onlyPrintDefaultValues)
--- hwt/synthesizer/rtlLevel/mainBases.py
+++ hwt/synthesizer/rtlLevel/mainBases.py
@@ -1,5 +1,6 @@
 class RtlSignalBase():
     pass
 
+
 class RtlMemoryBase(RtlSignalBase):
     pass
