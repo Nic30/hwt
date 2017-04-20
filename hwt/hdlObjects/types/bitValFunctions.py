48d88d505e0290c97508c63a6b40447a54f0a4d3
nic30 <nic30@seznam.cz>
2017-04-20 14:27:44 +0200

    codestyle: pep8

--- hwt/hdlObjects/types/bitValFunctions.py
+++ hwt/hdlObjects/types/bitValFunctions.py
@@ -9,29 +9,31 @@ from hwt.bitmask import mask
 
 BoolVal = BOOL.getValueCls()
 
+
 def bitsCmp__val(self, other, op, evalFn):
     w = self._dtype.bit_length()
     assert w == other._dtype.bit_length(), "%d, %d" % (w, other._dtype.bit_length())
-    
+
     vld = self.vldMask & other.vldMask
     _vld = vld == mask(w)
     res = evalFn(self.val, other.val) and _vld
     updateTime = max(self.updateTime, other.updateTime)
-    
+
     return BoolVal(res, BOOL, int(_vld), updateTime)
+
 
 def bitsCmp(self, other, op, evalFn=None):
     """
     :attention: If other is Bool signal convert this to boolean (not ideal, due VHDL event operator)
     """
     other = toHVal(other)
-    
+
     iamVal = isinstance(self, Value)
-    otherIsVal = isinstance(other, Value) 
+    otherIsVal = isinstance(other, Value)
 
     if evalFn is None:
         evalFn = op._evalFn
-    
+
     if iamVal and otherIsVal:
         return bitsCmp__val(self, other, op, evalFn)
     else:
@@ -40,31 +42,33 @@ def bitsCmp(self, other, op, evalFn=None):
         elif self._dtype == other._dtype:
             pass
         elif isinstance(other._dtype, Integer):
-            other = other._convert(self._dtype) 
+            other = other._convert(self._dtype)
         else:
             raise TypeError("Values of types (%s, %s) are not comparable" % (repr(self._dtype), repr(other._dtype)))
-        
-        return Operator.withRes(op, [self, other], BOOL) 
+
+        return Operator.withRes(op, [self, other], BOOL)
+
 
 def bitsBitOp__val(self, other, op, getVldFn):
     w = self._dtype.bit_length()
     assert w == other._dtype.bit_length()
-    
+
     vld = getVldFn(self, other)
     res = op._evalFn(self.val, other.val) & vld
     updateTime = max(self.updateTime, other.updateTime)
-    
+
     return self.__class__(res, self._dtype, vld, updateTime)
+
 
 def bitsBitOp(self, other, op, getVldFn):
     """
     :attention: If other is Bool signal, convert this to boolean (not ideal, due VHDL event operator)
     """
     other = toHVal(other)
-    
+
     iamVal = isinstance(self, Value)
-    otherIsVal = isinstance(other, Value) 
-    
+    otherIsVal = isinstance(other, Value)
+
     if iamVal and otherIsVal:
         return bitsBitOp__val(self, other, op, getVldFn)
     else:
@@ -74,30 +78,32 @@ def bitsBitOp(self, other, op, getVldFn):
         elif self._dtype == other._dtype:
             pass
         else:
-            raise TypeError("Types are not comparable (%s, %s)" % 
-                                       (repr(self._dtype), repr(other._dtype)))
-        
-        return Operator.withRes(op, [self, other], self._dtype) 
+            raise TypeError("Types are not comparable (%s, %s)" %
+                            (repr(self._dtype), repr(other._dtype)))
+
+        return Operator.withRes(op, [self, other], self._dtype)
+
 
 def bitsArithOp__val(self, other, op):
     v = self.clone()
     self_vld = self._isFullVld()
     other_vld = other._isFullVld()
-     
+
     v.val = op._evalFn(self.val, other.val)
 
     # [TODO] correct overflow detection for signed values
     w = v._dtype.bit_length()
     v.val &= mask(w)
-    
+
     # [TODO] value check range
     if self_vld and other_vld:
         v.vldMask = mask(w)
     else:
         v.vldMask = 0
-        
+
     v.updateTime = max(self.updateTime, other.updateTime)
     return v
+
 
 def bitsArithOp(self, other, op):
     other = toHVal(other)
@@ -109,14 +115,15 @@ def bitsArithOp(self, other, op):
         if self._dtype.signed is None:
             self = self._unsigned()
         if isinstance(other._dtype, Bits):
-            other = other._convSign(self._dtype.signed)    
+            other = other._convSign(self._dtype.signed)
         elif isinstance(other._dtype, Integer):
             pass
         else:
-            raise TypeError("%s %s %s" % (repr(self), repr(op) , repr(other)))
-        
+            raise TypeError("%s %s %s" % (repr(self), repr(op), repr(other)))
+
         o = Operator.withRes(op, [self, other], self._dtype)
         return o._convert(resT)
+
 
 def boundryFromType(sigOrVal, boundaryIndex):
     c = sigOrVal._dtype.constrain
@@ -125,10 +132,10 @@ def boundryFromType(sigOrVal, boundaryIndex):
     else:  # downto / to
         return c.singleDriver().ops[boundaryIndex]
 
+
 def getMulResT(firstT, secondT):
     if isinstance(secondT, Integer):
         return firstT # [maybe wrong]
-    
+
     width = firstT.bit_length() + secondT.bit_length()
     return vecT(width, firstT.signed)
-    
