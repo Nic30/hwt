47272c28fd2e2c9bb0004a5c31ef45f05bc31421
nic30 <nic30@seznam.cz>
2017-06-07 11:09:24 +0200

    codestyle: whitespaces

--- hwt/hdlObjects/frameTemplate.py
+++ hwt/hdlObjects/frameTemplate.py
@@ -5,11 +5,13 @@ from hwt.hdlObjects.transactionPart import TransactionPart
 from hwt.hdlObjects.types.array import Array
 from hwt.hdlObjects.types.bits import Bits
 from hwt.hdlObjects.types.struct import HStruct
+from hwt.simulator.types.simBits import simBitsT
+from hwt.bitmask import mask, selectBitRange, setBitRange
 
 
 def walkFlatten(transactionTmpl, offset=None, shouldEnterFn=lambda transTmpl: True):
     """
-    Walk fields in instance of TransactionTemplate
+    Walk fields in instance of TransTmpl
     :param shouldEnterFn: function (transTmpl) which returns True when field should
         be split on it's children
     """
@@ -45,36 +47,57 @@ def walkFlatten(transactionTmpl, offset=None, shouldEnterFn=lambda transTmpl: Tr
 
 class FrameTemplate(object):
     """
-    Container for informations about frame,
+    Frame template container for informations about frame,
     template which is used for resolving how data should be formated into words and frames
     on target interface
+
+    :ivar _fieldToTPart:
     """
     __RE_RM_ARRAY_DOTS = re.compile("(\.\[)")
 
-    def __init__(self, wordWidth, startBitAddr, endBitAddr, transactionParts):
+    def __init__(self, origin, wordWidth, startBitAddr, endBitAddr, transactionParts):
         """
+        :param origin: instance of HType (usually HStruct) from which this FrameTemplate was generated from
         :param wordWidth: width of word on interface where this template should be used
         :param startBitAddr: bit offset where this frame starts
         :param endBitAddr: bit offset where this frame ends (bit index of first bit behind this frame)
         :param transactionParts: instances of TransactionPart which are parts of this frame
         """
+        self.origin = origin
         self.wordWidth = wordWidth
         self.startBitAddr = startBitAddr
         self.endBitAddr = endBitAddr
         self.parts = transactionParts
 
+        self._fieldToTPart = None
+        
         for p in self.parts:
             p.parent = self
             assert p.startOfPart >= startBitAddr, (p, startBitAddr, self)
             assert p.endOfPart <= endBitAddr, (p, endBitAddr, self)
 
     @staticmethod
-    def framesFromTransactionTemplate(transactionTmpl,
-                                      wordWidth,
-                                      maxFrameLen=inf,
-                                      maxPaddingWords=inf,
-                                      trimPaddingWordsOnStart=False,
-                                      trimPaddingWordsOnEnd=False):
+    def frameFromTransTmpl(transactionTmpl,
+                           wordWidth,
+                           maxPaddingWords=inf,
+                           trimPaddingWordsOnStart=False,
+                           trimPaddingWordsOnEnd=False):
+        """
+        Params same as framesFromTransTmpl
+        """
+        return next(FrameTemplate.framesFromTransTmpl(transactionTmpl,
+                                                      wordWidth,
+                                                      maxPaddingWords=maxPaddingWords,
+                                                      trimPaddingWordsOnStart=trimPaddingWordsOnStart,
+                                                      trimPaddingWordsOnEnd=trimPaddingWordsOnEnd))
+    
+    @staticmethod
+    def framesFromTransTmpl(transactionTmpl,
+                            wordWidth,
+                            maxFrameLen=inf,
+                            maxPaddingWords=inf,
+                            trimPaddingWordsOnStart=False,
+                            trimPaddingWordsOnEnd=False):
         """
         Convert transaction template into FrameTemplates
 
@@ -251,6 +274,65 @@ class FrameTemplate(object):
 
             if parts:
                 yield (wIndex, parts)
+
+    @staticmethod
+    def buildFieldToDataDict(dtype, data, res):
+        # assert data is None or isinstance(data, dict)
+        for f in dtype.fields:
+            try:
+                fVal = data[f.name]
+            except KeyError:
+                fVal = None
+            
+            if isinstance(f.dtype, Bits):
+                if fVal is not None:
+                    assert isinstance(fVal, int)
+                    res[f] = fVal
+            elif isinstance(f.dtype, HStruct):
+                if fVal:
+                    FrameTemplate.buildFieldToDataDict(f.dtype, fVal, res)
+            elif isinstance(f.dtype, Array):
+                if fVal:
+                    # assert isinstance(fVal, class_or_tuple)
+                    res[f] = fVal
+        
+        return res
+    
+    def packData(self, data):
+        """
+        Pack data into list of BitsVal of specified dataWidth
+    
+        :param data: dict of values for struct fields {fieldName: value}
+    
+        :return: list of BitsVal which are representing values of words
+        """
+        typeOfWord = simBitsT(self.wordWidth, None)
+        fieldToVal = self._fieldToTPart
+        if fieldToVal is None:
+            fieldToVal = self._fieldToTPart = self.buildFieldToDataDict(self.origin, data)
+
+        for _, transactionParts in self.walkWords(showPadding=True):
+            actualVldMask = 0
+            actualVal = 0
+            for tPart in transactionParts:
+                high, low = tPart.getBusWordBitRange()
+                fhigh, flow = tPart.getFieldBitRange()
+                if not tPart.isPadding:
+                    val = fieldToVal.get(tPart.tmpl.origin, None)
+                else:
+                    val = None
+    
+                if val is None:
+                    newBits = 0
+                    vld = 0
+                else:
+                    newBits = selectBitRange(val, flow, fhigh - flow)
+                    vld = mask(high - low) << low
+    
+                setBitRange(actualVal, low, high - low, newBits)
+                setBitRange(actualVal, low, high - low, vld)
+                    
+            yield typeOfWord.getValueCls()(actualVal, typeOfWord, actualVldMask, -1)
 
     def __repr__getName(self, transactionPart, fieldWidth):
         if transactionPart.isPadding:
