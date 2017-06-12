192b551be6d9b380948fa5de320abb8d324a4044
nic30 <nic30@seznam.cz>
2017-06-12 20:48:04 +0200

    fix instantiation of aggents for StructIntf

--- hwt/interfaces/agents/structIntf.py
+++ hwt/interfaces/agents/structIntf.py
@@ -1,4 +1,5 @@
 from hwt.simulator.agentBase import AgentBase
+from hwt.synthesizer.interfaceLevel.interfaceUtils.proxy import InterfaceProxy
 
 
 class StructIntfAgent(AgentBase):
@@ -9,13 +10,32 @@ class StructIntfAgent(AgentBase):
     """
     def __init__(self, intf):
         AgentBase.__init__(self, intf)
+        # if interface is InterfaceProxy and this proxy is container of another array
+        # we generate agents on items in array
+        # otherwise generate agents for each subinterface
+        
         for intf in intf._interfaces:
-            intf._ag = intf._getSimAgent()(intf)
+            if isinstance(intf, InterfaceProxy) and intf._origIntf._arrayElemCache:
+                agCls = intf[0]._getSimAgent()
+                for p in intf:
+                    #print("arr", id(p), p._origIntf, p._offset, p._index)
+                    p._ag = agCls(p)
+            else:
+                #print("non-arr", id(intf), intf._origIntf, intf._offset, intf._index)
+                intf._ag = intf._getSimAgent()(intf)
 
     def getMonitors(self):
         for intf in self.intf._interfaces:
-            yield from intf._ag.getMonitors()
+            if isinstance(intf, InterfaceProxy) and intf._origIntf._arrayElemCache:
+                for p in intf:
+                    yield from p._ag.getMonitors()
+            else:
+                yield from intf._ag.getMonitors()
 
     def getDrivers(self):
         for intf in self.intf._interfaces:
-            yield from intf._ag.getDrivers()
+            if isinstance(intf, InterfaceProxy) and intf._origIntf._arrayElemCache:
+                for p in intf:
+                    yield from p._ag.getDrivers()
+            else:
+                yield from intf._ag.getDrivers()
