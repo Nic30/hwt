38510dc95ad102f204de86d2074ae803e8a2ad3c
nic30 <nic30@seznam.cz>
2017-06-14 16:57:00 +0200

    new sim utils pprintInterface, pprintAgents

--- hwt/simulator/utils.py
+++ hwt/simulator/utils.py
@@ -1,5 +1,8 @@
 from random import Random
 
+from hwt.serializer.serializerClases.indent import getIndent
+from hwt.synthesizer.interfaceLevel.interfaceUtils.proxy import InterfaceProxy
+
 
 def valueHasChanged(valA, valB):
     return valA.val is not valB.val or valA.vldMask != valB.vldMask
@@ -17,3 +20,46 @@ def agent_randomize(agent, timeQuantum, seed):
             yield simulator.wait(delay)
 
     return randomEnProc
+
+
+def pprintInterface(intf, prefix="", indent=0):
+    """
+    Pretty print interface
+    """
+    try:
+        s = intf._sig
+    except AttributeError:
+        s = ""
+    if s is not "":
+        s = repr(s)
+    
+    print("".join([getIndent(indent), prefix, repr(intf._getFullName()), " ", s]))
+    for i in intf._interfaces:
+        if isinstance(intf, InterfaceProxy):
+            assert isinstance(i, InterfaceProxy), (intf, i)
+        pprintInterface(i, indent=indent + 1)
+    
+    if intf._arrayElemCache:
+        assert len(intf) == len(intf._arrayElemCache)
+        for i, p in enumerate(intf):
+            pprintInterface(p, prefix="p%d:" % i, indent=indent + 1)
+    
+def _pprintAgents(intf, indent, prefix=""):
+    if intf._ag is not None:
+        print("%s%s%r" % (getIndent(indent), prefix, intf._ag))
+    for i in intf._interfaces:
+        _pprintAgents(i, indent + 1)
+
+    if intf._arrayElemCache:
+        assert len(intf) == len(intf._arrayElemCache)
+        for i, p in enumerate(intf):
+            _pprintAgents(p, indent + 1, prefix="p%d:" % i)
+        
+    
+def pprintAgents(unitOrIntf, indent=0):
+    """
+    Pretty print agents
+    """
+    prefix = unitOrIntf._name + ":"
+    for intf in unitOrIntf._interfaces:
+        _pprintAgents(intf, indent, prefix)
