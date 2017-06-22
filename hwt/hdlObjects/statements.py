a3fac0bec69282e863bbf88f27df215a0b209e52
nic30 <nic30@seznam.cz>
2017-06-23 00:08:03 +0200

    migrate ReturnCalled and ReturnContainer to hwtHdlParsers

--- hwt/hdlObjects/statements.py
+++ hwt/hdlObjects/statements.py
@@ -1,24 +1,4 @@
 
-class ReturnCalled(Exception):
-    """
-    Exception which is used as return statement while executing of hdl functions
-    """
-    def __init__(self, val):
-        self.val = val
-
-
-class ReturnContainer():
-    """
-    Stuctural container of return statement in hdl
-    """
-
-    def __init__(self, val=None):
-        self.val = val
-
-    def seqEval(self):
-        raise ReturnCalled(self.val.staticEval())
-
-
 def seqEvalCond(cond):
     _cond = True
     for c in cond:
