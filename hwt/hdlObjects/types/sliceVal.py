d5bb7b140ff4b48b41b5992e93bae9c547558ef8
nic30 <nic30@seznam.cz>
2017-06-23 19:39:27 +0200

    rm unused SliceVal.staticEval

--- hwt/hdlObjects/types/sliceVal.py
+++ hwt/hdlObjects/types/sliceVal.py
@@ -1,15 +1,11 @@
 from hwt.hdlObjects.types.arrayVal import ArrayVal
-from hwt.hdlObjects.types.defs import SLICE
 from hwt.hdlObjects.value import Value
 
 
 class SliceVal(ArrayVal):
-
-    def staticEval(self):
-        _0 = self.val[0].staticEval()
-        _1 = self.val[1].staticEval()
-        updateTime = max(_0.updateTime, _1.updateTime)
-        return SliceVal([_0, _1], SLICE, None, updateTime)
+    """
+    Value class for Slice type
+    """
 
     def _isFullVld(self):
         return self.val[0]._isFullVld() and self.val[1]._isFullVld()
