f0327b205648b0c788a3462da19880cef8214ada
nic30 <nic30@seznam.cz>
2017-06-24 12:13:53 +0200

    optimalize getBaseCond

--- hwt/synthesizer/termUsageResolver.py
+++ hwt/synthesizer/termUsageResolver.py
@@ -7,14 +7,15 @@ def getBaseCond(c):
     if is negated return original cond and negated flag
     """
     isNegated = False
-    drivers = []
     try:
         drivers = c.drivers
     except AttributeError:
-        pass
+        return (c, isNegated)
+
     if len(drivers) == 1:
         d = list(c.drivers)[0]
         if isinstance(d, Operator) and d.operator == AllOps.NOT:
             c = d.ops[0]
             isNegated = True
+
     return (c, isNegated)
