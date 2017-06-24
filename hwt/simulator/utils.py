10d67eac249f392c00a3d9665b02b1ce454393aa
nic30 <nic30@seznam.cz>
2017-06-24 13:52:32 +0200

    testable pprintInterface and pprintAgents

--- hwt/simulator/utils.py
+++ hwt/simulator/utils.py
@@ -1,4 +1,5 @@
 from random import Random
+import sys
 
 from hwt.serializer.serializerClases.indent import getIndent
 from hwt.synthesizer.interfaceLevel.interfaceUtils.proxy import InterfaceProxy
@@ -22,7 +23,7 @@ def agent_randomize(agent, timeQuantum, seed):
     return randomEnProc
 
 
-def pprintInterface(intf, prefix="", indent=0):
+def pprintInterface(intf, prefix="", indent=0, file=sys.stdout):
     """
     Pretty print interface
     """
@@ -32,34 +33,37 @@ def pprintInterface(intf, prefix="", indent=0):
         s = ""
     if s is not "":
         s = repr(s)
-    
-    print("".join([getIndent(indent), prefix, repr(intf._getFullName()), " ", s]))
+
+    file.write("".join([getIndent(indent), prefix, repr(intf._getFullName()), " ", s]))
+    file.write("\n")
+
     for i in intf._interfaces:
         if isinstance(intf, InterfaceProxy):
             assert isinstance(i, InterfaceProxy), (intf, i)
-        pprintInterface(i, indent=indent + 1)
-    
-    if intf._arrayElemCache:
-        assert len(intf) == len(intf._arrayElemCache)
-        for i, p in enumerate(intf):
-            pprintInterface(p, prefix="p%d:" % i, indent=indent + 1)
-    
-def _pprintAgents(intf, indent, prefix=""):
-    if intf._ag is not None:
-        print("%s%s%r" % (getIndent(indent), prefix, intf._ag))
-    for i in intf._interfaces:
-        _pprintAgents(i, indent + 1)
+        pprintInterface(i, indent=indent + 1, file=file)
 
     if intf._arrayElemCache:
         assert len(intf) == len(intf._arrayElemCache)
         for i, p in enumerate(intf):
-            _pprintAgents(p, indent + 1, prefix="p%d:" % i)
-        
-    
-def pprintAgents(unitOrIntf, indent=0):
+            pprintInterface(p, prefix="p%d:" % i, indent=indent + 1, file=file)
+
+
+def _pprintAgents(intf, indent, prefix="", file=sys.stdout):
+    if intf._ag is not None:
+        file.write("%s%s%r\n" % (getIndent(indent), prefix, intf._ag))
+    for i in intf._interfaces:
+        _pprintAgents(i, indent + 1, file=file)
+
+    if intf._arrayElemCache:
+        assert len(intf) == len(intf._arrayElemCache)
+        for i, p in enumerate(intf):
+            _pprintAgents(p, indent + 1, prefix="p%d:" % i, file=file)
+
+
+def pprintAgents(unitOrIntf, indent=0, file=sys.stdout):
     """
     Pretty print agents
     """
     prefix = unitOrIntf._name + ":"
     for intf in unitOrIntf._interfaces:
-        _pprintAgents(intf, indent, prefix)
+        _pprintAgents(intf, indent, prefix, file=file)
