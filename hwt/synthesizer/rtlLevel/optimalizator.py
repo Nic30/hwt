a2f09fd4ec5f45ec87e26a707b2ca5da5c414e57
nic30 <nic30@seznam.cz>
2017-06-26 14:22:07 +0200

    fix removing of unused registers

--- hwt/synthesizer/rtlLevel/optimalizator.py
+++ hwt/synthesizer/rtlLevel/optimalizator.py
@@ -1,20 +1,55 @@
+from hwt.hdlObjects.operator import Operator
+from hwt.hdlObjects.assignment import Assignment
+from hwt.hdlObjects.value import Value
 
 
 def removeUnconnectedSignals(netlist):
+    """
+    If signal is not driving anything remove it
+    """
+
     toDelete = set()
-    while True:
-        for sig in netlist.signals:
+    toSearch = netlist.signals
+
+    while toSearch:
+        _toSearch = set()
+
+        for sig in toSearch:
             if not sig.endpoints:
                 try:
                     if sig._interface is not None:
+                        # skip interfaces before we want to check them,
+                        # they should not be optimized out from design
                         continue
                 except AttributeError:
                     pass
+
+                for e in sig.drivers:
+                    # drivers of this signal are useless rm them
+                    if isinstance(e, Operator):
+                        for op in e.ops:
+                            if not isinstance(op, Value):
+                                op.endpoints.remove(e)
+                                _toSearch.add(op)
+
+                    elif isinstance(e, Assignment):
+                        op = e.src
+                        if not isinstance(op, Value):
+                            op.endpoints.remove(e)
+                            _toSearch.add(op)
+                    
+                        netlist.startsOfDataPaths.remove(e)
+                    else:
+                        assert False, ("Drivers should be only index operators or assignments", e)
+
                 toDelete.add(sig)
 
         if toDelete:
             for sig in toDelete:
                 netlist.signals.remove(sig)
+                try:
+                    _toSearch.remove(sig)
+                except KeyError:
+                    pass
             toDelete = set()
-        else:
-            return
+        toSearch = _toSearch
