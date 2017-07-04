bc6445ab8351b597738c6c1b2ef91134813c604d
nic30 <nic30@seznam.cz>
2017-07-04 21:21:50 +0200

    verilog: add newline after always stm.

--- hwt/serializer/verilog/templates/process.v
+++ hwt/serializer/verilog/templates/process.v
@@ -1,4 +1,5 @@
 {% if hasToBeVhdlProcess %}{{indent}}{% if sensitivityList|length>0 %}always @({{ sensitivityList}}){% else %}always_comb
 {%endif%} begin: {{ name }}{% for s in statements %}
 {{s}};{% endfor %}
-{{indent}}end{% else %}{% for s in statements %}{{s}}; {% endfor %}{% endif %}
+{{indent}}end
+{% else %}{% for s in statements %}{{s}}; {% endfor %}{% endif %}
