55d6a3c98d400b1d8935c670e298c30cb79ea370
nic30 <nic30@seznam.cz>
2017-07-05 14:54:37 +0200

    verilog fix component instance name

--- hwt/serializer/verilog/templates/component_instance.v
+++ hwt/serializer/verilog/templates/component_instance.v
@@ -1,5 +1,5 @@
-{{indent}}{{ entity.name }} {{ instanceName }}{% if genericMaps|length >0 %} #({{genericMaps|join(',\n    ' + indent)}} 
-{{indent}}    ){% endif %}{% if portMaps|length >0 %} ({{portMaps|join(',\n    ' + indent)}}
+{{indent}}{{ entity.name }} {% if genericMaps|length >0 %} #({{genericMaps|join(',\n    ' + indent)}} 
+{{indent}}    ){% endif %}{{ instanceName }}{% if portMaps|length >0 %} ({{portMaps|join(',\n    ' + indent)}}
 {{indent}}    );
 {% endif %}
 
