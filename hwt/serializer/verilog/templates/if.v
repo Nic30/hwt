2d23fe4d15bec58b63c4800864308781c17ed7ed
nic30 <nic30@seznam.cz>
2017-07-07 19:28:55 +0200

    verilog: fix redundand end in if statement

--- hwt/serializer/verilog/templates/if.v
+++ hwt/serializer/verilog/templates/if.v
@@ -1,7 +1,7 @@
-{{indent}}if({{ cond }}) {% if ifTrue|length >0 %} begin
+{{indent}}if({{ cond }}){% if ifTrue|length >0 %} begin
 {% for s in ifTrue %}{{s}}
-{%endfor%}{{indent}}end{% endif %}{% if elIfs|length >0 %}{% for c, stms in elIfs %}{{indent}}end else if({{c}}) begin
+{%endfor%}{{indent}}end{% endif %}{% if elIfs|length >0 %}{% for c, stms in elIfs %} else if({{c}}) begin
 {% for s in stms %}{{s}}
 {%endfor%}{%endfor%}{{indent}}end{% endif %}{% if ifFalse|length >0 %} else begin
 {%for s in ifFalse %}{{s}}
-{% endfor %}{% endif %}{{indent}}end
+{% endfor %}{{indent}}end{% endif %}
