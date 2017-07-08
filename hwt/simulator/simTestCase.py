a6c99a8ea4d4646f7a96d211c5758158affe02a1
nic30 <nic30@seznam.cz>
2017-07-08 19:32:07 +0200

    cleanup SimTestCase

--- hwt/simulator/simTestCase.py
+++ hwt/simulator/simTestCase.py
@@ -1,10 +1,7 @@
 from _random import Random
-import difflib
 from inspect import isgenerator
 import os
-import pprint
 import unittest
-from unittest.util import safe_repr, _common_shorten_repr
 
 from hwt.hdlObjects.constants import Time
 from hwt.hdlObjects.types.arrayVal import ArrayVal
@@ -124,90 +121,7 @@ class SimTestCase(unittest.TestCase):
             differences.
         """
         seq1 = allValuesToInts(seq1)
-        if seq_type is not None:
-            seq_type_name = seq_type.__name__
-            if not isinstance(seq1, seq_type):
-                raise self.failureException('First sequence is not a %s: %s'
-                                            % (seq_type_name, safe_repr(seq1)))
-            if not isinstance(seq2, seq_type):
-                raise self.failureException('Second sequence is not a %s: %s'
-                                            % (seq_type_name, safe_repr(seq2)))
-        else:
-            seq_type_name = "sequence"
-
-        differing = None
-        try:
-            len1 = len(seq1)
-        except (TypeError, NotImplementedError):
-            differing = 'First %s has no length.    Non-sequence?' % (
-                    seq_type_name)
-
-        if differing is None:
-            try:
-                len2 = len(seq2)
-            except (TypeError, NotImplementedError):
-                differing = 'Second %s has no length.    Non-sequence?' % (
-                        seq_type_name)
-
-        if differing is None:
-            if seq1 == seq2:
-                return
-
-            differing = '%ss differ: %s != %s\n' % (
-                    (seq_type_name.capitalize(),) + 
-                    _common_shorten_repr(seq1, seq2))
-
-            for i in range(min(len1, len2)):
-                try:
-                    item1 = seq1[i]
-                except (TypeError, IndexError, NotImplementedError):
-                    differing += ('\nUnable to index element %d of first %s\n'
-                                  % (i, seq_type_name))
-                    break
-
-                try:
-                    item2 = seq2[i]
-                except (TypeError, IndexError, NotImplementedError):
-                    differing += ('\nUnable to index element %d of second %s\n'
-                                  % (i, seq_type_name))
-                    break
-
-                if item1 != item2:
-                    differing += ('\nFirst differing element %d:\n%s\n%s\n'
-                                  % ((i,) + _common_shorten_repr(item1, item2)))
-                    break
-            else:
-                if (len1 == len2 and seq_type is None and
-                   type(seq1) != type(seq2)):
-                    # The sequences are the same, but have differing types.
-                    return
-
-            if len1 > len2:
-                differing += ('\nFirst %s contains %d additional '
-                              'elements.\n' % (seq_type_name, len1 - len2))
-                try:
-                    differing += ('First extra element %d:\n%s\n' % 
-                                  (len2, safe_repr(seq1[len2])))
-                except (TypeError, IndexError, NotImplementedError):
-                    differing += ('Unable to index element %d '
-                                  'of first %s\n' % (len2, seq_type_name))
-            elif len1 < len2:
-                differing += ('\nSecond %s contains %d additional '
-                              'elements.\n' % (seq_type_name, len2 - len1))
-                try:
-                    differing += ('First extra element %d:\n%s\n' % 
-                                  (len1, safe_repr(seq2[len1])))
-                except (TypeError, IndexError, NotImplementedError):
-                    differing += ('Unable to index element %d '
-                                  'of second %s\n' % (len1, seq_type_name))
-        standardMsg = differing
-        diffMsg = '\n' + '\n'.join(
-            difflib.ndiff(pprint.pformat(seq1).splitlines(),
-                          pprint.pformat(seq2).splitlines()))
-
-        standardMsg = self._truncateMessage(standardMsg, diffMsg)
-        msg = self._formatMessage(msg, standardMsg)
-        self.fail(msg)
+        self.assertSequenceEqual(seq1, seq2, msg, seq_type)
 
     def randomize(self, intf):
         """
