f813de3c3d6a2f0b5ed8f6a0750f1d1230c3649c
nic30 <nic30@seznam.cz>
2017-07-11 23:37:22 +0200

    arrayQuery: put back take

--- hwt/pyUtils/arrayQuery.py
+++ hwt/pyUtils/arrayQuery.py
@@ -74,6 +74,22 @@ def arr_all(iterable, fn):
     return True
 
 
+def take(iterrable, howMay):
+    """
+    :return: generator of first n items from iterrable
+    """
+    assert howMay >= 0
+
+    if not howMay:
+        return
+
+    last = howMay - 1
+    for i, item in enumerate(iterrable):
+        yield item
+        if i == last:
+            return
+
+
 def where(iterable, fn):
     """
     :return: generator of items from iterable where fn(item)
