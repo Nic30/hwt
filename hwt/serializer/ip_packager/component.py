5cf41e0f578612c1394379b2b03d29c99559caa4
nic30 <nic30@seznam.cz>
2017-07-11 22:38:23 +0200

    ipcore packager: comment out fromElm methods on xml object (not tested)

--- hwt/serializer/ip_packager/component.py
+++ hwt/serializer/ip_packager/component.py
@@ -23,7 +23,8 @@ class Component():
     """
     _strValues = ["vendor", "library", "name", "version", "description"]
     # _iterableValues = ["fileSets", "parameters" ]
-    def __init__(self): 
+
+    def __init__(self):
         self.vendor = ''
         self.library = ''
         self.name = ""
@@ -36,7 +37,7 @@ class Component():
         self.vendorExtensions = VendorExtensions()
         self._files = []
         self._topUnit = None
-                      
+
     # @classmethod
     # def load(cls, xmlStr):
     #    self = cls()
@@ -44,7 +45,7 @@ class Component():
     #        etree.register_namespace(prefix, uri)
     #    self.root = etree.fromstring(xmlStr)
     #    self.name = findS(self.root, "name").text
-    #    
+    #
     #    intf = findS(self.root, "busInterfaces")
     #    self.interfaces = {}
     #    for i in intf:
@@ -53,9 +54,9 @@ class Component():
     #            raise Exception("Multiple interfaces with same name")
     #        self.interfaces[i.name] = i
     #    self.model = Model.fromElem(findS(self.root, "model"))
-    #    
+    #
     #    return self
-    
+
     def _xmlFileSets(self, componentElem):
         def fileSetFromFiles(name, files):
             fileSet = FileSet()
@@ -66,7 +67,7 @@ class Component():
             return fileSet
         filesets = appendSpiElem(componentElem, "fileSets")
         hdlExtensions = [".vhd", 'v']
-        
+
         hdl_fs = fileSetFromFiles(vhdl_syn_fileSetName,
                                   whereEndsWithExts(self._files, hdlExtensions))
         hdl_sim_fs = fileSetFromFiles(vhdl_sim_fileSetName,
@@ -75,12 +76,12 @@ class Component():
                                       whereEndsWithExt(self._files, ".tcl"))
         for fs in [hdl_fs, hdl_sim_fs, tclFileSet]:
             filesets.append(fs.asElem())
-        
+
     def _xmlParameters(self, compElem):
         parameters = appendSpiElem(compElem, "parameters")
         for p in self.parameters:
             parameters.append(p.asElem())
-        
+
     def xml(self):
         # Vivado 2015.2 bug - order of all elements is NOT optional
         for prefix, uri in ns.items():
@@ -92,16 +93,16 @@ class Component():
             for intf in self.busInterfaces:  # for all interfaces which have bus interface class
                 if hasattr(intf, "_bi"):
                     bi.append(intf._bi.asElem())
-        
+
         c.append(self.model.asElem())
         self._xmlFileSets(c)
-        
+
         appendStrElements(c, self, [self._strValues[-1]])
         self._xmlParameters(c)
         c.append(self.vendorExtensions.asElem(self.name + "_v" + self.version, revision=str(int(time()))))
-        
+
         return c
-    
+
     def asignTopUnit(self, unit):
         """
         Set hwt unit as template for component
@@ -115,8 +116,8 @@ class Component():
         for intf in unit._interfaces:
             if intf._isExtern:
                 self.busInterfaces.append(intf)
-                
-        self.busInterfaces.sort(key=lambda x : x._name)
+
+        self.busInterfaces.sort(key=lambda x: x._name)
         for intf in self.busInterfaces:
             biClass = None
             try:
@@ -127,8 +128,7 @@ class Component():
                 bi = BusInterface.fromBiClass(intf, biClass)
                 intf._bi = bi
                 bi.busType.postProcess(self, self._topUnit, self.busInterfaces, intf)
-        
-        
+
         # generate component parameters
         compNameParam = Parameter()
         compNameParam.name = "Component_Name"
@@ -136,14 +136,14 @@ class Component():
         v = compNameParam.value
         v.id = "PARAM_VALUE.Component_Name"
         v.resolve = "user"
-        v.text = self.name    
+        v.text = self.name
         self.parameters.append(compNameParam)
         # generic as parameters
         for g in self._topUnit._entity.generics:
             p = Parameter()
             p.name = g.name
             p.value = Value.fromGeneric("PARAM_VALUE.", g, Value.RESOLVE_USER)
-            self.parameters.append(p)    
+            self.parameters.append(p)
 
         # for bi in self.busInterfaces:
         #    bi.name = trimUnderscores(bi.name)
@@ -151,7 +151,3 @@ class Component():
         #        p.name = removeUndescores_witSep(p.name, ".")
         #        p.value.id = removeUndescores_witSep(p.value.id , ".")
         #        p.value.text = removeUndescores_witSep(p.value.text , ".")
-
-            
-
-
