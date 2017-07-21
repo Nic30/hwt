38bf3d55c0e4305c683f405b3fc54a00ec47c483
nic30 <nic30@seznam.cz>
2017-07-21 15:32:04 +0200

    verilogTypeOfSig const as reg

--- hwt/serializer/verilog/utils.py
+++ hwt/serializer/verilog/utils.py
@@ -1,3 +1,4 @@
+from hwt.hdlObjects.operator import Operator
 from hwt.hdlObjects.portItem import PortItem
 from hwt.hdlObjects.types.enum import Enum
 from hwt.pyUtils.arrayQuery import arr_any
@@ -11,8 +12,8 @@ def verilogTypeOfSig(signalItem):
     """
     Check if is register or wire
     """
-    if len(signalItem.drivers) > 1 or\
-       arr_any(signalItem.drivers, lambda d: not isinstance(d, PortItem) and d.isEventDependent) or\
+    if signalItem._const or len(signalItem.drivers) > 1 or\
+       arr_any(signalItem.drivers, lambda d: not isinstance(d, (PortItem, Operator)) and d.isEventDependent) or\
        (signalItem._useNopVal and len(signalItem.drivers) > 0 and signalItem.drivers[0].cond):
 
         return SIGNAL_TYPE.REG
