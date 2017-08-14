2eaf93126813b0d00d8b78d6aa0cd052f04f6e51
nic30 <nic30@seznam.cz>
2017-08-14 12:26:15 +0200

    v1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='hwt',
-      version='1.7',
+      version='1.8',
       description='hdl synthesis toolkit',
       url='https://github.com/Nic30/HWToolkit',
       author='Michal Orsak',
