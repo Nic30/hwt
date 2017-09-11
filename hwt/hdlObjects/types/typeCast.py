4509593fd718a1981c93025e0730d095b2a493db
nic30 <nic30@seznam.cz>
2017-09-11 17:27:08 +0200

    toHVal: add suggested type param

--- hwt/hdlObjects/types/typeCast.py
+++ hwt/hdlObjects/types/typeCast.py
@@ -1,5 +1,8 @@
 
+from typing import Optional
+
 from hwt.hdlObjects.types.defs import INT, STR, BOOL
+from hwt.hdlObjects.types.hdlType import HdlType
 from hwt.hdlObjects.value import Value
 from hwt.hdlObjects.variables import SignalItem
 from hwt.synthesizer.interfaceLevel.mainBases import InterfaceBase
@@ -10,14 +13,17 @@ defaultConversions = {int: INT,
                       bool: BOOL}
 
 
-def toHVal(op):
-    """Convert python value object to object of hdl type value"""
+def toHVal(op, suggestedType: Optional[HdlType]=None):
+    """Convert python or hdl value/signal object to hdl value/signal object"""
     if isinstance(op, Value) or isinstance(op, SignalItem):
         return op
     elif isinstance(op, InterfaceBase):
         return op._sig
     else:
         if isinstance(op, int):
+            if suggestedType is not None:
+                return suggestedType.fromPy(op)
+
             if op >= 1 << 31:
                 raise TypeError("Number %d is too big to fit in 32 bit integer of HDL use Bits type instead" % op)
             elif op < -(1 << 31):
