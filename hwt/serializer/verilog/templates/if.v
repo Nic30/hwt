628639dcac14aa3ec201d94b83bbbdd0bc212a5d
nic30 <nic30@seznam.cz>
2018-02-17 00:04:24 +0100

    fix verilog elif indent

--- hwt/serializer/verilog/templates/if.v
+++ hwt/serializer/verilog/templates/if.v
@@ -1,7 +1,18 @@
-{{indent}}if({{ cond }}){% if ifTrue|length >0 %} begin
-{% for s in ifTrue %}{{s}}
-{%endfor%}{{indent}}end{% endif %}{% if elIfs|length >0 %}{% for c, stms in elIfs %} else if({{c}}) begin
-{% for s in stms %}{{s}}
-{%endfor%}{%endfor%}{{indent}}end{% endif %}{% if ifFalse|length >0 %} else begin
-{%for s in ifFalse %}{{s}}
-{% endfor %}{{indent}}end{% endif %}
+{{indent}}if({{ cond }}){%
+if ifTrue|length >0 %} begin
+{%    for s in ifTrue %}{{s}}
+{%
+    endfor%}{{indent}}end{% 
+endif %}{% 
+if elIfs|length >0 %}{% 
+    for c, stms in elIfs 
+       %} else if({{c}}) begin
+{%     for s in stms %}{{s}}
+{%     endfor%}{{indent}}end{%
+    endfor%}{% 
+endif %}{% 
+if ifFalse|length >0 %} else begin
+{%  for s in ifFalse %}{{s}}
+{%  endfor 
+%}{{indent}}end{% 
+endif %}
