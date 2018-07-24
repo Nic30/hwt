cd01a67f5898b392873f3318bb3d26fd8e8624e5
Nic30 <nic30@seznam.cz>
2018-07-24 14:22:53 +0200

    v2.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='hwt',
-      version='2.3',
+      version='2.4',
       description='hdl synthesis toolkit',
       long_description=long_description,
       long_description_content_type="text/markdown",
