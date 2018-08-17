e7a8c17f4f46d968b88c357ce7367c795cdb6d4e
Nic30 <nic30@seznam.cz>
2018-08-17 13:07:05 +0200

    DifferentialSig fix type of "n" signal

--- hwt/interfaces/differential.py
+++ hwt/interfaces/differential.py
@@ -1,8 +1,10 @@
+from hwt.hdl.types.bits import Bits
 from hwt.interfaces.std import Signal
 from hwt.synthesizer.interface import Interface
 
 
 class DifferentialSig(Interface):
+
     def _declr(self):
-        self.n = Signal()
+        self.n = Signal(dtype=Bits(negated=1))
         self.p = Signal()
