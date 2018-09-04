f06e701e2cb5596bfd56111a98293403a210f37d
Nic30 <nic30@seznam.cz>
2018-09-04 17:39:47 +0200

    codestyle

--- hwt/hdl/types/stream.py
+++ hwt/hdl/types/stream.py
@@ -4,10 +4,10 @@ from hwt.hdl.types.hdlType import HdlType
 class HStream(HdlType):
     """
     Stream is an abstract type. It is an array with unspecified size.
-    
+
     :ivar elmType: type of elements
     """
-    
+
     def __init__(self, elmType):
         super(HStream, self).__init__()
         self.elmType = elmType
