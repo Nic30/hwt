35e2df094ae1edcc2b2e3a87b46d896b704b6baf
Nic30 <nic30@seznam.cz>
2018-09-04 17:10:39 +0200

    fix HObjList type errors

--- hwt/synthesizer/hObjList.py
+++ hwt/synthesizer/hObjList.py
@@ -4,13 +4,14 @@ from hwt.synthesizer.interfaceLevel.mainBases import InterfaceBase
 class HObjList(list):
     """
     Regular list with some interface/unit methods delegated on items.
-    
+
     Main purpose of this class it let :class:`hwt.synthesizer.PropDeclrCollector.PropDeclrCollector`
     know that this is not an regular python array and that items should be registered as HW objects.
-    
+
     :note: :class:`hwt.synthesizer.PropDeclrCollector.PropDeclrCollector` is used by
         :class:`hwt.synthesizer.interface.Interface` and :class:`hwt.synthesizer.unit.Unit`
     """
+
     def __init__(self, *args, **kwargs):
         list.__init__(self, *args, **kwargs)
         self._name = None
@@ -39,29 +40,32 @@ class HObjList(list):
             else:
                 tmp = None
         return name
-    
+
     def _make_association(self, *args, **kwargs):
         """
         Delegate _make_association on items
-        
+
         :note: doc in :func:`~hwt.synthesizer.interfaceLevel.propDeclCollector._make_association`
         """
         for o in self:
             o._make_association(*args, **kwargs)
-    
+
     def _updateParamsFrom(self, *args, **kwargs):
         """
         :note: doc in :func:`~hwt.synthesizer.interfaceLevel.propDeclCollector._updateParamsFrom`
         """
         for o in self:
             o._updateParamsFrom(*args, **kwargs)
-    
+
     def __call__(self, other):
-        if not isinstance(other, HObjList) or len(self) != len(other):
-            return TypeError()
-        
+        if not isinstance(other, list):
+            raise TypeError(other)
+        if len(self) != len(other):
+            raise ValueError("Different number of interfaces in list",
+                             len(self), len(other))
+
         statements = []
         for a, b in zip(self, other):
             statements += a(b)
-        
+
         return statements
