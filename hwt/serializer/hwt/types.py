a4d25582c0825145c0adcd66b371a121425fc69d
Nic30 <nic30@seznam.cz>
2018-12-02 17:36:18 +0100

    add missing definition of HwtSerializer methods for types

--- hwt/serializer/hwt/types.py
+++ hwt/serializer/hwt/types.py
@@ -1,14 +1,38 @@
 from hwt.hdl.types.integerVal import IntegerVal
 from hwt.synthesizer.param import evalParam
-from hwt.serializer.simModel.types import SimModelSerializer_types
 from hwt.hdl.types.bits import BITS_DEFAUTL_SIGNED, BITS_DEFAUTL_FORCEVECTOR,\
     BITS_DEFAUTL_NEGATED
 
 
-class HwtSerializer_types(SimModelSerializer_types):
+class HwtSerializer_types():
     """
     part of SimModelSerializer responsible for type serialization
     """
+
+    @classmethod
+    def HdlType_bool(cls, typ, ctx, declaration=False):
+        assert not declaration
+        return "BOOL"
+
+    @classmethod
+    def HdlType_enum(cls, typ, ctx, declaration=False):
+        if declaration:
+            typ.name = ctx.scope.checkedName(typ.name, typ)
+
+            return '%s = HEnum( "%s", [%s])' % (
+                typ.name,
+                typ.name,
+                ", ".join(map(lambda x: '"%s"' % x,
+                              typ._allValues)))
+        else:
+            return typ.name
+
+    @classmethod
+    def HdlType_array(cls, typ, ctx, declaration=False):
+        assert not declaration
+        return "HArray(%s, %d)" % (cls.HdlType(typ.elmType, ctx,
+                                               declaration=declaration),
+                                   evalParam(typ.size).val)
 
     @classmethod
     def HdlType_int(cls, typ, ctx, declaration):
