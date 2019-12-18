bd6a57f448e19e30371c98e5cce5085ae16bb77e
Nic30 <nic30@seznam.cz>
2019-12-18 09:56:36 +0100

    verilog: always @(*) codestyle

--- hwt/serializer/verilog/templates/process.v
+++ hwt/serializer/verilog/templates/process.v
@@ -2,8 +2,7 @@
    %}{{indent}}{% 
     if sensitivityList|length>0 
          %}always @({{ sensitivityList}}){% 
-    else %}always @(*)
-{%  endif%} begin: {{ name }}{% 
+    else %}always @(*){%  endif%} begin: {{ name }}{% 
     if extraVars %}{%
         for ev in extraVars%}
 {{           indent}}{{ev}};{%      endfor%}{% 
