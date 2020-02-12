a4b6fc8e8ca34ea5feb4ac2889b0ab6255981f2a
Nic30 <nic30@seznam.cz>
2020-02-12 21:29:43 +0100

    HStream: start alignments

--- hwt/hdl/types/stream.py
+++ hwt/hdl/types/stream.py
@@ -1,3 +1,6 @@
+from math import inf, isinf
+from typing import List, Optional
+
 from hwt.hdl.types.hdlType import HdlType
 
 
@@ -5,12 +8,33 @@ class HStream(HdlType):
     """
     Stream is an abstract type. It is an array with unspecified size.
 
-    :ivar element_t: type of elements
+    :ivar element_t: type of smalest chunk of data
+        which can be send over this stream
+    :ivar len_min: minimum repetitions of element_t (inclusive interval)
+    :ivar len_max: maximum repetitions of element_t (inclusive interval)
+    :ivar start_offsets: list of numbers which represents the number of invalid bytes
+        before valid data on stream (invalid bytes means the bytes
+        which does not have bit validity set, e.g. Axi4Stream keep=0b10 -> offset=1 
+        )
     """
 
-    def __init__(self, element_t):
+    def __init__(self, element_t,
+                 frame_len=inf,
+                 start_offsets: Optional[List[int]]=None):
         super(HStream, self).__init__()
         self.element_t = element_t
+        if isinstance(frame_len, float) and isinf(frame_len):
+            frame_len = (0, inf)
+        elif isinstance(frame_len, int):
+            frame_len = (frame_len, frame_len)
+        self.len_min, self.len_max = frame_len
+        if start_offsets is None:
+            start_offsets = [0, ]
+        self.start_offsets = start_offsets
 
     def bit_length(self):
-        raise TypeError("Stream does not have constant size")
+        if self.len_min != self.len_max or isinf(self.len_max):
+            raise TypeError("HStream does not have constant size", self)
+        else:
+            # len_min == len_max
+            raise self.len_min * self.element_t.bit_length()
