598744b9517ed906b466ce887d3c9ba2423cf01d
Nic30 <nic30@seznam.cz>
2020-04-20 18:53:17 +0200

    HWProcess -> HdlStatementBlock,proc is hdlConvertor.hdlAst.HdlStmProcess

--- hwt/hdl/block.py
+++ hwt/hdl/block.py
@@ -1,33 +1,27 @@
 from typing import List, Set
 
-from hwt.hdl.hdlObject import HdlObject
+from hwt.hdl.statement import HdlStatement
 from hwt.pyUtils.uniqList import UniqList
 
 
-class HWProcess(HdlObject):
+class HdlStatementBlock(HdlStatement):
     """
-    Hdl process container
+    Hdl block statement used also to represent a HDL process
 
     :ivar ~.name: name used as id in target HDL
     :ivar ~.statements: list of statements in body of process
-    :ivar ~.sensitivityList: set of RtlSignals or event operators which
-        are describing when should this process be re-evaluated
 
-    :ivar ~.inputs: all input signals for this process
-    :ivar ~.outputs: all output signals for this process
-    :ivar ~.count: of branches in statements, used as hint for process complexity
-
-    :note: HWProcess do not have to be process in target HDL, for example
+    :note: HdlStatementBlock do not have to be process in target HDL, for example
         simple process which contains only unconditional assignment will
         be rendered just as assignment
     """
 
     def __init__(self, name: str, statements: List['HdlStatement'],
-                 sensitivityList: Set["RtlSignal"],
+                 sensitivity: Set["RtlSignal"],
                  inputs: UniqList, outputs: UniqList):
+        super(HdlStatementBlock, self).__init__(sensitivity=sensitivity)
         self.name = name
         self.statements = statements
-        self.sensitivityList = sensitivityList
-        self.inputs = inputs
-        self.outputs = outputs
+        self._inputs = inputs
+        self._outputs = outputs
         self.rank = sum(map(lambda s: s.rank, statements))
