ed94e5ee0cd93658595854630881ad7079ca5905
Nic30 <nic30@seznam.cz>
2020-05-09 21:11:51 +0200

    fix error on unregistered hdl ids in repr of hdl objects

--- hwt/hdl/hdlObject.py
+++ hwt/hdl/hdlObject.py
@@ -10,7 +10,7 @@ class HdlObject():
 
     def __repr__(self):
         from hwt.serializer.hwt import HwtSerializer
-        name_scope = NameScope(None, "debug", False)
+        name_scope = NameScope(None, "debug", False, debug=True)
         to_hdl = HwtSerializer.TO_HDL_AST(name_scope)
         to_hdl.debug = True
         hdl = to_hdl.as_hdl(self)
