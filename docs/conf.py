69012a870df096070ccdef2ab4e96fbe535d215e
Nic30 <nic30@seznam.cz>
2020-12-17 17:37:05 +0100

    doc: add intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -34,11 +34,18 @@ sys.path.insert(0, os.path.abspath('../'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.todo',
-              'sphinx.ext.viewcode',
-              # 'sphinx.ext.napoleon',
-              'sphinx.ext.graphviz']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    # 'sphinx.ext.napoleon',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'ipCorePackager': ("https://ipcorepackager.readthedocs.io/en/latest/", None),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
