cf5ed1d3ce8cf75cb3ede263aaf74db232f90e99
Nic30 <nic30@seznam.cz>
2020-12-24 03:52:52 +0100

    removeUnconnectedSignals: simplify nop value handling

--- hwt/synthesizer/rtlLevel/remove_unconnected_signals.py
+++ hwt/synthesizer/rtlLevel/remove_unconnected_signals.py
@@ -1,7 +1,34 @@
 from hwt.doc_markers import internal
+from hwt.hdl.assignment import Assignment
+from hwt.hdl.ifContainter import IfContainer
 from hwt.hdl.operator import Operator
+from hwt.hdl.portItem import HdlPortItem
+from hwt.hdl.statement import HdlStatement
+from hwt.hdl.switchContainer import SwitchContainer
 from hwt.synthesizer.rtlLevel.mainBases import RtlSignalBase
-from hwt.synthesizer.rtlLevel.constants import NOT_SPECIFIED
+from ipCorePackager.constants import DIRECTION
+from hwt.synthesizer.interfaceLevel.interfaceUtils.utils import walkPhysInterfaces
+
+
+@internal
+def walkInputsForSpecificOutput(output_sig: RtlSignalBase, stm: HdlStatement):
+    if output_sig not in stm._outputs:
+        return
+    if isinstance(stm, Assignment):
+        assert stm.dst is output_sig
+        yield from stm._inputs
+    elif isinstance(stm, IfContainer):
+        yield stm.cond
+        for c, _ in stm.elIfs:
+            yield c
+        for _stm in stm._iter_stms():
+            yield from walkInputsForSpecificOutput(output_sig, _stm)
+    elif isinstance(stm, SwitchContainer):
+        yield stm.switchOn
+        for _stm in stm._iter_stms():
+            yield from walkInputsForSpecificOutput(output_sig, _stm)
+    else:
+        raise NotImplementedError(stm)
 
 
 @internal
@@ -11,57 +38,53 @@ def removeUnconnectedSignals(netlist: "RtlNetlist"):
 
     :attention: does not remove signals in cycles which does not affect outputs
     """
-
-    toDelete = set()
-    toSearch = netlist.signals
-    # nop value to it's target signals
-    nop_values = {}
-    for sig in netlist.signals:
-        if isinstance(sig._nop_val, RtlSignalBase):
-            nop_values.setdefault(sig._nop_val, set()).add(sig)
+    toSearch = [s for s, d in netlist.interfaces.items() if d != DIRECTION.IN]
+    seen = set(toSearch)
+    for c in netlist.subUnits:
+        toSearch.extend(sig._sig for sig in walkPhysInterfaces(c))
 
     while toSearch:
-        _toSearch = set()
+        _toSearch = []
         for sig in toSearch:
-            if not sig.endpoints:
-                try:
-                    if sig._interface is not None:
-                        # skip interfaces before we want to check them,
-                        # they should not be optimized out from design
-                        continue
-                except AttributeError:
-                    pass
+            for e in sig.drivers:
+                if isinstance(e, Operator):
+                    inputs = e.operands
+                elif isinstance(e, HdlPortItem):
+                    # we are already added inputs of all components
+                    continue
+                else:
+                    inputs = walkInputsForSpecificOutput(sig, e)
+                for i in inputs:
+                    if isinstance(i, RtlSignalBase) and i not in seen:
+                        seen.add(i)
+                        _toSearch.append(i)
 
-                for e in tuple(sig.drivers):
-                    # drivers of this signal are useless rm them
-                    if isinstance(e, Operator):
-                        inputs = e.operands
-                        removed_e = e
-                    else:
-                        removed_e = e._cut_off_drivers_of(sig)
-                        inputs = removed_e._inputs
-
-                    for op in inputs:
-                        if isinstance(op, RtlSignalBase):
-                            _toSearch.add(op)
-
-                    if removed_e is not None:
-                        # must not destroy before procesing inputs
-                        removed_e._destroy()
-
-                toDelete.add(sig)
-                if sig._nop_val in netlist.signals:
-                    _toSearch.add(sig._nop_val)
-
-        if toDelete:
-            for sig in toDelete:
-                if sig.ctx == netlist:
-                    netlist.signals.remove(sig)
-                _toSearch.discard(sig)
-            toDelete = set()
+            nv = sig._nop_val
+            if isinstance(nv, RtlSignalBase):
+                if nv not in seen:
+                    seen.add(nv)
+                    _toSearch.append(nv)
         toSearch = _toSearch
 
-    for sig, dst_sigs in nop_values.items():
-        if sig not in netlist.signals:
-            for dst in dst_sigs:
-                dst._nop_val = NOT_SPECIFIED
+    seen.update([s for s, d in netlist.interfaces.items() if d == DIRECTION.IN])
+    for c in netlist.subUnits:
+        seen.update(sig._sig for sig in walkPhysInterfaces(c))
+
+    for sig in netlist.signals:
+        if sig in seen:
+            continue
+
+        for e in tuple(sig.drivers):
+            # drivers of this signal are useless rm them
+            if isinstance(e, Operator):
+                removed_e = e
+            elif isinstance(e, HdlPortItem):
+                raise NotImplementedError(sig)
+            else:
+                removed_e = e._cut_off_drivers_of(sig)
+
+            if removed_e is not None:
+                # must not destroy before procesing inputs
+                removed_e._destroy()
+
+    netlist.signals = seen
