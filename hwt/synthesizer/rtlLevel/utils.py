ba78eb8a2b015bd5993ceab5a4700294f922597c
Nic30 <nic30@seznam.cz>
2021-01-22 14:29:19 +0100

    cleanup

--- hwt/synthesizer/rtlLevel/utils.py
+++ hwt/synthesizer/rtlLevel/utils.py
@@ -6,12 +6,4 @@ from hwt.hdl.variables import SignalItem
 
 @internal
 def portItemfromSignal(s: SignalItem, component, d: DIRECTION):
-    pi = HdlPortItem(s.name, d, s._dtype, component)
-    # if not hasattr(s, '_interface'):
-    #     # dummy signal for the case we are converting a netlist which is not wrapped in :class:`hwt.synthesizer.unit.Unit` instance
-    #     from hwt.interfaces.std import Signal
-    #     t = s._dtype
-    #     s._interface = Signal(dtype=t)
-    # pi._interface = s._interface
-
-    return pi
+    return HdlPortItem(s.name, d, s._dtype, component)
