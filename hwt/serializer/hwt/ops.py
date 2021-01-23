613e126db82645d747ba7f4e83ca9805ca825a34
Nic30 <nic30@seznam.cz>
2021-01-23 20:42:14 +0100

    codestyle, rm duplicit code

--- hwt/serializer/hwt/ops.py
+++ hwt/serializer/hwt/ops.py
@@ -1,24 +1,17 @@
-from hdlConvertorAst.hdlAst._expr import HdlOpType, HdlOp, HdlValueId
-from hdlConvertorAst.translate._verilog_to_basic_hdl_sim_model.utils import hdl_getattr,\
+from hdlConvertorAst.hdlAst._expr import HdlOp, HdlValueId
+from hdlConvertorAst.translate._verilog_to_basic_hdl_sim_model.utils import hdl_getattr, \
     hdl_call
 from hdlConvertorAst.translate.common.name_scope import LanguageKeyword
 from hwt.hdl.operator import Operator
 from hwt.hdl.operatorDefs import AllOps
-from hwt.serializer.generic.ops import HWT_TO_HDLCONVEROTR_OPS
 from hwt.serializer.hwt.context import ValueWidthRequirementScope
+from hwt.serializer.simModel.value import ToHdlAstSimModel_value
 
 
 class ToHdlAstHwt_ops():
     CONCAT = HdlValueId("Concat", obj=LanguageKeyword())
-    op_transl_dict = {
-        **HWT_TO_HDLCONVEROTR_OPS,
-        AllOps.INDEX: HdlOpType.INDEX,
-    }
-    _cast_ops = {
-        AllOps.BitsAsSigned,
-        AllOps.BitsAsUnsigned,
-        AllOps.BitsAsVec,
-    }
+    op_transl_dict = ToHdlAstSimModel_value.op_transl_dict
+    _cast_ops = ToHdlAstSimModel_value._cast_ops
 
     def as_hdl_Operator(self, op: Operator):
         ops = op.operands
