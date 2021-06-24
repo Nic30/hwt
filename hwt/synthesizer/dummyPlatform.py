26247bef5268c3e68c10b33225cc788ec97dfe8b
Nic30 <nic30@seznam.cz>
2021-06-24 08:46:49 +0200

    doc: DummyPlatform

--- hwt/synthesizer/dummyPlatform.py
+++ hwt/synthesizer/dummyPlatform.py
@@ -1,10 +1,14 @@
 from hwt.synthesizer.rtlLevel.extract_part_drivers import extract_part_drivers
-from hwt.synthesizer.rtlLevel.remove_unconnected_signals import removeUnconnectedSignals
 from hwt.synthesizer.rtlLevel.mark_visibility_of_signals_and_check_drivers import markVisibilityOfSignalsAndCheckDrivers
+from hwt.synthesizer.rtlLevel.remove_unconnected_signals import removeUnconnectedSignals
 
 
 class DummyPlatform():
     """
+    Dummy synthesis platform, base class of all chip and toolset specific platforms.
+    Plaform in this context is a set of configurations which do describe the target toolset and chip/node.
+    It can also contains pre/post processing callbacks and optimizations required for this target.
+
     :note: all processors has to be callable with only one parameter
         which is actual Unit/RtlNetlist instance
     """
