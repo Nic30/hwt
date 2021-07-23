7fbce30331b9a528f298be1c2ca14d3b28795fcf
Nic30 <nic30@seznam.cz>
2021-07-23 16:07:35 +0200

    use HwtDebugSerializer in __repr__ so errors are always readable

--- hwt/hdl/hdlObject.py
+++ hwt/hdl/hdlObject.py
@@ -9,14 +9,14 @@ class HdlObject():
     """
 
     def __repr__(self):
-        from hwt.serializer.hwt import HwtSerializer
+        from hwt.serializer.hwt import HwtDebugSerializer
         name_scope = NameScope(None, "debug", False, debug=True)
-        to_hdl = HwtSerializer.TO_HDL_AST(name_scope)
+        to_hdl = HwtDebugSerializer.TO_HDL_AST(name_scope)
         to_hdl.debug = True
         hdl = to_hdl.as_hdl(self)
         buff = StringIO()
         # import sys
         # buff = sys.stdout
-        ser = HwtSerializer.TO_HDL(buff)
+        ser = HwtDebugSerializer.TO_HDL(buff)
         ser.visit_iHdlObj(hdl)
         return buff.getvalue()
--- hwt/serializer/hwt/__init__.py
+++ hwt/serializer/hwt/__init__.py
@@ -4,9 +4,25 @@ Hwt serializer converts  HDL objects back to code in python for hwt.
 from hdlConvertorAst.to.hwt._main import ToHwt
 from hwt.serializer.hwt.serializer import ToHdlAstHwt
 from hwt.serializer.serializer_config import DummySerializerConfig
+from hdlConvertorAst.hdlAst import iHdlObj
 
 
 class HwtSerializer(DummySerializerConfig):
     fileExtension = '.py'
     TO_HDL_AST = ToHdlAstHwt
+    TO_HDL = ToHwt
+
+
+class ToHdlAstDebugHwt(ToHdlAstHwt):
+
+    def as_hdl(self, obj) -> iHdlObj:
+        try:
+            return super(ToHdlAstDebugHwt, self).as_hdl(obj)
+        except:
+            return obj.__repr__()
+
+
+class HwtDebugSerializer(DummySerializerConfig):
+    fileExtension = '.py'
+    TO_HDL_AST = ToHdlAstDebugHwt
     TO_HDL = ToHwt
