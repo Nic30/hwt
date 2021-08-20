d7d5ea7bd749d9e3ef382c971b3549c6b083fb67
Nic30 <nic30@seznam.cz>
2021-08-20 21:04:50 +0200

    codestyle: UniqList typing

--- hwt/pyUtils/uniqList.py
+++ hwt/pyUtils/uniqList.py
@@ -1,18 +1,24 @@
+from typing import Generic, TypeVar, Set, Sequence, Optional
+
+T = TypeVar('T')
 
 
-class UniqList(list):
+class UniqList(Generic[T], list):
     """
     List of unique items
     """
 
-    def __init__(self, initSeq=None):
+    def __init__(self, initSeq: Optional[Sequence[T]]=None):
         super(UniqList, self).__init__()
-        self.__s = set()
+        self.__s: Set[T] = set()
         if initSeq is not None:
             for item in initSeq:
                 self.append(item)
 
-    def append(self, item):
+    def append(self, item: T) -> bool:
+        """
+        :returns: True if the item was in list already
+        """
         if item in self.__s:
             return False
         else:
@@ -20,32 +26,35 @@ class UniqList(list):
             list.append(self, item)
             return True
 
-    def extend(self, items):
+    def extend(self, items: Sequence[T]):
         for item in items:
             self.append(item)
 
-    def insert(self, i, x):
+    def insert(self, i: int, x: T):
         super(UniqList, self).insert(i, x)
         self.__s.add(x)
 
-    def _get_set(self):
+    def _get_set(self) -> Set[T]:
         return self.__s
 
-    def intersection_set(self, other):
+    def intersection_set(self, other: Set[T]):
         return self.__s.intersection(other._get_set())
 
-    def discard(self, item):
+    def discard(self, item: T) -> bool:
+        """
+        :returns: True if the item was in list already
+        """
         if item in self.__s:
             self.remove(item)
             return True
         else:
             return False
 
-    def remove(self, item):
+    def remove(self, item: T):
         self.__s.remove(item)
         return list.remove(self, item)
 
-    def pop(self, *args, **kwargs):
+    def pop(self, *args, **kwargs) -> T:
         item = list.pop(self, *args, **kwargs)
         self.__s.remove(item)
         return item
@@ -62,5 +71,5 @@ class UniqList(list):
     def __copy__(self):
         return self.copy()
 
-    def __contains__(self, key):
+    def __contains__(self, key) -> bool:
         return key in self.__s
