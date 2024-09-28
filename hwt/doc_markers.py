63a58a96c52c2250f7dda5da87c365baac0903ea
Nic30 <nic30@seznam.cz>
2024-09-29 00:34:51 +0200

    feat(doc_markers): new decorator hwt_expr_producer

--- hwt/doc_markers.py
+++ hwt/doc_markers.py
@@ -6,3 +6,10 @@ def internal(fn):
     which tells that this object is not interesting for users and it is only used internally 
     """
     return fn
+
+def hwt_expr_producer(fn):
+    """
+    Decorator which does not affect functionality.
+    For documentation purposes it specifies that the function produces hwt expression.
+    """
+    return fn
